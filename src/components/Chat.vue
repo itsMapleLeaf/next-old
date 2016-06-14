c5c035a5e005f478daddd67aa0ec745887a7b1a0
Kingdaro <kingdaro@gmail.com>
2016-06-14 12:31:36 -0400

    better styling

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,31 +1,34 @@
 <template>
-  <div class='container'>
-    <div class='row row-2'>
-      <div class='col-2 fg-color center-content'>
-        <i class='fa fa-bars'></i>
+  <div>
+    <div class='grid'>
+      <div class='row row-2'>
+        <div class='col-2 fg-color center-content'>
+          <i class='fa fa-bars'></i>
+        </div>
+        <div class='row grow padded center-content-vertical'>
+          tab bar
+        </div>
       </div>
-      <div class='row grow padded center-content-vertical'>
-        tab bar
-      </div>
-    </div>
 
-    <div class='row row-6 fg-color padded'>room info</div>
-
-    <div class='divider'></div>
-
-    <div class='row grow'>
-      <div class='grow padded'>chat message area</div>
+      <div class='row row-6 fg-color padded'>room info</div>
 
       <div class='divider'></div>
 
-      <div class='col-10 fg-color padded'>character list</div>
-    </div>
+      <div class='row grow'>
+        <div class='grow padded'>chat message area</div>
 
-    <div class='divider'></div>
+        <div class='divider'></div>
 
-    <div class='row row-6 fg-color'>
-      <chatbox></chatbox>
+        <div class='col-10 fg-color padded'>character list</div>
+      </div>
+
+      <div class='divider'></div>
+
+      <div class='row row-6 fg-color'>
+        <chatbox></chatbox>
+      </div>
     </div>
+    <router-view></router-view>
   </div>
 </template>
 
