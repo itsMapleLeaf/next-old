f71974bba29bb95e6966185970a0438a7efb610f
Kingdaro <kingdaro@gmail.com>
2016-06-14 15:51:45 -0400

    added search box

--- src/components/ChannelList.vue
+++ src/components/ChannelList.vue
@@ -2,11 +2,21 @@
   <div class='overlay-shade'>
     <div class='panel material-shadow'>
       <h1>Channel List</h1>
-      <selection-list>
-        <selection-list-item v-for='info in getAllChannels.slice(0, 200)'>
-          {{ info.title || info.name }}
-        </selection-list-item>
-      </selection-list>
+      <form>
+        <fieldset>
+          <selection-list>
+            <selection-list-item v-for='info in filteredChannels'>
+              {{ info.title || info.name }}
+            </selection-list-item>
+          </selection-list>
+        </fieldset>
+        <fieldset>
+          <input type="text" placeholder="Search..." v-model='searchQuery'>
+        </fieldset>
+        <fieldset>
+          <button>Done</button>
+        </fieldset>
+      </form>
     </div>
   </div>
 </template>
@@ -15,11 +25,35 @@
 import SelectionList from './SelectionList.vue'
 import SelectionListItem from './SelectionListItem.vue'
 import {getAllChannels} from '../vuex/getters'
+import fuzzysearch from 'fuzzysearch'
 
 export default {
   components: {
     SelectionList,
     SelectionListItem
+  },
+
+  data () {
+    return {
+      searchQuery: '',
+    }
+  },
+
+  computed: {
+    filteredChannels () {
+      if (this.searchQuery !== '') {
+        const filter = info => fuzzysearch(this.searchQuery, info.title || info.name)
+        return this.getAllChannels.filter(filter).slice(0, 200)
+      } else {
+        return this.getAllChannels.slice(0, 200)
+      }
+    }
+  },
+
+  methods: {
+    matchesSearch (info) {
+      return fuzzysearch(this.searchQuery, info.title || info.name)
+    }
   },
 
   vuex: {
