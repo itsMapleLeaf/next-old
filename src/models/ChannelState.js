2312d7033f748f12e78e03eaa9d500f8b21092ac
Kingdaro <kingdaro@gmail.com>
2016-06-15 13:59:35 -0400

    added ChatMessage model, + type annotations!

--- src/models/ChannelState.js
+++ src/models/ChannelState.js
@@ -1,10 +1,11 @@
 export default function ChannelState (id, name) {
   return {
-    id, name,
-    status: 'joining',
-    mode: 'both',
-    description: '',
-    characters: [],
-    messages: []
+    id,                // string: the channel id (either 'channel' or 'name' from the server)
+    name,              // string: the channel name (or 'title' from the server)
+    status: 'joining', // enum: 'joining' | 'joined' | 'leaving'
+    mode: 'both',      // enum: 'both' | 'chat' | 'ads'
+    description: '',   // string
+    characters: [],    // Character[] (see models/Character.js)
+    messages: []       // ChatMessage[] (see models/ChatMessage.js)
   }
 }
--- src/models/Character.js
+++ src/models/Character.js
@@ -1,3 +1,8 @@
 export default function Character (name, gender, status = 'online', statusMessage = '') {
-  return { name, gender, status, statusMessage }
+  return {
+    name,          // string
+    gender,        // enum: 'Male' | 'Female' | 'Transgender' | 'Herm' | 'Shemale' | 'Cunt-boy' | 'Male-herm' | 'None'
+    status,        // enum: 'online' | 'busy' | 'away' | 'dnd' | 'idle' | 'offline'
+    statusMessage, // string
+  }
 }
