27abe7009fdab6801ae6b5154f6ecc059502a047
Kingdaro <kingdaro@gmail.com>
2016-06-21 18:48:59 -0400

    defragment the chat view template

--- src/components/ChatView.vue
+++ src/components/ChatView.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class='header box fg-color scroll'>
-    <slot name='header'></slot>
-  </div>
-
-  <div class='box divider'></div>
-
-  <div class='box horizontal grow'>
-    <div class='box grow scroll'>
-      <slot name='content'></slot>
+  <div class='box vertical grow'>
+    <div class='header box fg-color scroll'>
+      <slot name='header'></slot>
     </div>
 
-    <div class='sidebar box fg-color scroll'>
-      <slot name='sidebar'></slot>
-    </div>
-  </div>
+    <div class='box divider'></div>
 
-  <div class='box divider'></div>
+    <div class='box horizontal grow'>
+      <div class='box grow scroll'>
+        <slot name='content'></slot>
+      </div>
+
+      <div class='sidebar box fg-color scroll'>
+        <slot name='sidebar'></slot>
+      </div>
+    </div>
+
+    <div class='box divider'></div>
+  </div>
 </template>
 
 <style lang="stylus">
