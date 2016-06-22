79f17b0a462aec8810080eb4a87a5bb587c33a6a
Kingdaro <kingdaro@gmail.com>
2016-06-22 04:46:49 -0400

    no need for this
    for now anyway

--- src/vuex/getters.js
+++ src/vuex/getters.js
@@ -2,15 +2,6 @@ import {ChannelState} from '../models'
 
 export const userData = state => Object.assign({}, state.userData)
 
-export const userCharacters = state =>
-  state.loginData.characters
-    .slice()
-    .sort()
-
-export const userAccount = state => state.account
-
-export const userCharacter = state => state.character
-
 export const publicChannels = state => state.publicChannels.slice()
 
 export const privateChannels = state => state.privateChannels.slice()
