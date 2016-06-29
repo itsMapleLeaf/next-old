4d15d429cb3ab8fe434f6bc3b7c21659bc92eb63
Kingdaro <kingdaro@gmail.com>
2016-06-29 02:29:29 -0400

    styled character select

--- src/components/CharacterSelect.vue
+++ src/components/CharacterSelect.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="overlay-shade flex-center">
-    <div class="overlay-panel text-align-center">
-      <form @submit.prevent='submit'>
-        <h1>Who are you?</h1>
-        <ul class='selection-list'>
-          <li v-for='name in characters'
-          :class='{ "selected": name === selectedCharacter }'
-          @click='setSelectedCharacter(name)'>
-            {{name}}
-          </li>
-        </ul>
-        <br />
-        <button>Go</button>
+  <div class="ui overlay">
+    <div class="ui panel">
+      <h1>Who are you?</h1>
+      <form class='ui form' @submit.prevent='submit'>
+        <div class='ui field'>
+          <ul class='ui selection'>
+            <li v-for='name in characters'
+            :class='{ "selected": name === selectedCharacter }'
+            @click='setSelectedCharacter(name)'>
+              {{name}}
+            </li>
+          </ul>
+        </div>
+        <div class='ui field'>
+          <button class='ui button'>Go</button>
+        </div>
       </form>
     </div>
   </div>
