c048eddba3cd1c312c76b07ad450146221247228
Kingdaro <kingdaro@gmail.com>
2016-07-21 20:04:06 -0400

    character browser is about as functional as it needs to be minus a search bar + filters and junk

--- vue/components/CharacterBrowser.vue
+++ vue/components/CharacterBrowser.vue
@@ -1,69 +1,46 @@
 <template>
-  <div transition='fade'>
-    <div class='ui-overlay-shade'></div>
-    <div class='character-list ui-scroll'>
-      <div class='character ui-color-main ui-border ui-raised' v-for='char in characterList'>
-        <div class='image ui-color-dark flex-fixed'>
-          <img :src='char.avatarURL' />
-        </div>
-        <div class='character-info flex-stretch'>
-          <h3 class='name flex-fixed'>{{ char.name }}</h3>
-          <small class='status flex-stretch'>
-            <span>{{char.status}}</span>
-            <span v-if='char.statusmsg' v-html="' - ' + char.statusmsg | bbcode"></span>
-          </small>
-        </div>
-      </div>
+  <div class='container ui-overlay-shade ui-scroll' transition='fade'>
+    <div class='character-list'>
+      <card v-for='char in groups.friends' class='character' :character='char'></card>
+      <card v-for='char in groups.bookmarks' class='character' :character='char'></card>
+      <card v-for='char in groups.looking' class='character' :character='char'></card>
+      <card v-for='char in groups.rest' class='character' :character='char'></card>
     </div>
   </div>
 </template>
 
 <style lang="stylus" scoped>
-.character-list
-  padding-top: 1em
-  display: flex
-  flex-wrap: wrap
-  justify-content: center
+.container
   position: absolute
   height: 100vh
   width: 100vw
+  padding-top: 1em
+
+.character-list
+  display: flex
+  flex-wrap: wrap
+  justify-content: center
+  align-items: flex-start
 
 .character
   box-sizing: content-box
   width: 15em
-  height: 100px
+  height: min-content
   margin-right: 1em
   margin-bottom: 1em
   display: flex
-
-  img
-    display: block
-
-.character-info
-  display: flex
-  flex-direction: column
-  word-wrap: break-word
-
-.name, .status
-  padding: 0.3em 0.6em
-
-.name
-  margin: 0
-
-.status
-  overflow: hidden
-  text-overflow: ellipsis
-  overflow-wrap: break-word
-  font-style: italic
 </style>
 
 <script>
 import Overlay from './Overlay.vue'
-import {getAvatarURL} from '../modules/flist'
+import Card from './CharacterBrowserCard.vue'
 import {bbcode} from '../modules/filters'
+import {groupSort, compareByField} from '../modules/common'
+
+const compareName = compareByField('name')
 
 export default {
-  components: {Overlay},
+  components: {Overlay, Card},
 
   vuex: {
     getters: {
@@ -72,17 +49,24 @@ export default {
   },
 
   computed: {
-    characterList () {
-      return Object.values(this.characterMap)
-        .slice(0, 100)
-        .map(char => {
-          return {
-            name: char.name,
-            status: char.status,
-            statusmsg: char.statusMessage,
-            avatarURL: getAvatarURL(char.name)
-          }
-        })
+    groups () {
+      const characters = Object.values(this.characterMap)
+      const sorted = groupSort(characters, char => {
+        switch (true) {
+          case char.getFriends().length > 0: return 'friends'
+          case char.isBookmarked(): return 'bookmarks'
+          case char.status === 'looking': return 'looking'
+          default: return 'rest'
+        }
+      })
+
+      for (let group in sorted) {
+        sorted[group] = sorted[group]
+          .slice(0, 200)
+          .sort(compareName)
+      }
+
+      return sorted
     }
   },
 
---
+++ vue/components/CharacterBrowserCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <a href='#' class='ui-color-main ui-border ui-raised' :data-character-action='character.name'>
+    <div class='avatar ui-color-dark flex-fixed'>
+      <img :src='avatarURL' />
+    </div>
+    <div class='character-info flex-stretch'>
+      <h3 class='name flex-fixed ui-color-darker'>
+        {{character.name}}
+      </h3>
+      <small class='status flex-stretch'>
+        <span>{{character.status}}</span>
+        <span v-if='character.statusmsg' v-html="' - ' + character.statusmsg | bbcode"></span>
+      </small>
+    </div>
+  </a>
+</template>
+
+<style lang="stylus">
+a > *
+  pointer-events: none
+
+.avatar
+  padding: 0.5em
+
+  img
+    display: block
+
+.character-info
+  display: flex
+  flex-direction: column
+  word-wrap: break-word
+
+.name, .status
+  padding: 0.3rem 0.6rem
+
+.name
+  margin: 0
+
+.status
+  overflow: hidden
+  text-overflow: ellipsis
+  overflow-wrap: break-word
+  font-style: italic
+</style>
+
+<script>
+import Character from '../types/Character'
+import {getAvatarURL} from '../modules/flist'
+
+export default {
+  props: {
+    character: Character
+  },
+
+  computed: {
+    avatarURL () {
+      return getAvatarURL(this.character.name)
+    }
+  }
+}
+</script>
