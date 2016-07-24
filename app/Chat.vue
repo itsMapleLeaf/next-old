76ff29f15b47a21ea25c7ed99e8b4af4e8a37085
Kingdaro <kingdaro@gmail.com>
2016-07-24 03:48:55 -0400

    i hate css

--- app/Chat.vue
+++ app/Chat.vue
@@ -2,8 +2,13 @@
   <div class='chat ui-fullscreen ui-color-dark flex-column'>
     <div class='flex-fixed ui-height-1'>test</div>
     <div class='flex-fixed ui-height-2'>test</div>
-    <div class='flex-grow ui-scroll-y'>
-      <span v-for='i in 100'>fuck<br /></span>
+    <div class='flex-grow flex flex-align-stretch'>
+      <div class='flex-grow ui-scroll-y'>
+        <span v-for='i in 100'>fuck<br /></span>
+      </div>
+      <div class='ui-width-3 ui-scroll-y'>
+        <span v-for='i in 100'>fuck<br /></span>
+      </div>
     </div>
     <div class='flex-fixed ui-height-2'>test</div>
   </div>
