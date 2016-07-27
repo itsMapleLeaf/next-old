cbed993c51ab94ad2bbe3b65a6b4b13283496396
Kingdaro <kingdaro@gmail.com>
2016-07-27 11:52:34 -0400

    more transitions

--- app/view/RoomTab.vue
+++ app/view/RoomTab.vue
@@ -1,9 +1,11 @@
 <template>
-  <a href='#'
-    class='flex flex-align-center ui-padding-subtle ui-width-5 ui-divide-left'
-    :class="active ? 'color-main' : 'color-dark ui-hover-darken'">
-    <span class='ui-ellipsis'><slot></slot></span>
-  </a>
+  <transition name='fade'>
+    <a href='#'
+      class='flex flex-align-center ui-padding-subtle ui-width-5 ui-divide-left'
+      :class="active ? 'color-main' : 'color-dark ui-hover-darken'">
+      <span class='ui-ellipsis'><slot></slot></span>
+    </a>
+  </transition>
 </template>
 
 <script>
