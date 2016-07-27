a153d65c980071f238d1e548f03abf8afdfa25b2
Kingdaro <kingdaro@gmail.com>
2016-07-27 10:20:59 -0400

    functional menu header

--- app/view/MenuHeader.vue
+++ app/view/MenuHeader.vue
@@ -1,19 +1,25 @@
 <template>
   <section>
     <header class='ui-margin-bottom-1'>
-      <h2>Hi, Trance.</h2>
+      <h2>Hi, {{firstName}}!</h2>
       <small class='ui-faded'>In the mood for some play?</small>
     </header>
     <span class='ui-margin-v-1'>
-      <avatar name="Trance-"></avatar>
+      <avatar :name='identity'></avatar>
     </span>
   </section>
 </template>
 
 <script>
 import Avatar from './CharacterAvatarLink.vue'
+import store from '../store'
 
 export default {
-  components: {Avatar}
+  components: {Avatar},
+
+  computed: {
+    identity () { return store.identity },
+    firstName () { return this.identity.split(' ')[0] }
+  }
 }
 </script>
