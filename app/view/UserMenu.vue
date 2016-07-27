c5c112e3ef5ad703425c944c2abda9ecb16db079
Kingdaro <kingdaro@gmail.com>
2016-07-27 08:17:27 -0400

    add a mockup for some menu options

--- app/view/UserMenu.vue
+++ app/view/UserMenu.vue
@@ -2,7 +2,10 @@
   <div class='ui-overlay'>
     <div class='ui-panel-left ui-width-6 flex-column ui-scroll-y'>
       <section class='flex-fixed ui-padding-5'>
-        <h2 class='ui-margin-bottom-1'>Hi, Trance.</h2>
+        <header class='ui-margin-bottom-1'>
+          <h2>Hi, Trance.</h2>
+          <small class='ui-faded'>In the mood for some play?</small>
+        </header>
         <span class='ui-margin-v-1'>
           <avatar name="Trance-"></avatar>
         </span>
@@ -16,11 +19,20 @@
         </div>
       </section>
       <section class='flex-grow color-dark'>
-        <a class='ui-block ui-padding-4' href='#'><i class='mdi mdi-forum'></i> Channels</a>
-        <a class='ui-block ui-padding-4' href='#'><i class='mdi mdi-heart'></i> Online Users</a>
-        <a class='ui-block ui-padding-4' href='#'><i class='mdi mdi-settings'></i> Settings</a>
-        <a class='ui-block ui-padding-4' href='#'><i class='mdi mdi-logout'></i> Log Out</a>
-        <a class='ui-block ui-padding-4' href='#'><i class='mdi mdi-account-switch'></i> Change Character</a>
+        <a href='#' class='ui-block ui-padding-4'><i class='mdi mdi-earth'></i> Frontpage</a>
+        <a href='#' class='ui-block ui-padding-4 highlight-blue'><i class='mdi mdi-earth'></i> Fantasy</a>
+
+        <a href='#' class='ui-block ui-padding-4'><i class='mdi mdi-key-variant'></i> Furry Femboys</a>
+
+        <a href='#' class='ui-block ui-padding-4'><i class='mdi mdi-heart'></i> Lyara Silverwing</a>
+
+        <div class='ui-padding-1 color-darker'></div>
+
+        <a href='#' class='ui-block ui-padding-4'><i class='mdi mdi-forum'></i> Channels</a>
+        <a href='#' class='ui-block ui-padding-4'><i class='mdi mdi-heart'></i> Online Users</a>
+        <a href='#' class='ui-block ui-padding-4'><i class='mdi mdi-settings'></i> Settings</a>
+        <a href='#' class='ui-block ui-padding-4'><i class='mdi mdi-logout'></i> Log Out</a>
+        <a href='#' class='ui-block ui-padding-4'><i class='mdi mdi-account-switch'></i> Change Character</a>
       </section>
     </div>
   </div>
