9e5e290ed954f8eefe66e8bfa1bf48f77c0be935
Kingdaro <kingdaro@gmail.com>
2016-07-27 06:47:12 -0400

    basic rendering of current chat

--- app/view/Chat.vue
+++ app/view/Chat.vue
@@ -1,24 +1,21 @@
 <template>
-  <div class='chat color-dark flex-column'>
-    <description class='flex-fixed color-main ui-height-2 ui-desktop'>
-      {{description}}
-    </description>
-    <div class='flex-grow flex flex-align-stretch ui-divide-top'>
-      <messages
-        class='flex-grow ui-scroll-y'
-        :messages='messages'>
-      </messages>
-      <users
-        class='ui-desktop flex-fixed ui-width-5 color-main ui-scroll-y ui-divide-left'
-        :users='users'>
-      </users>
-    </div>
-    <chatbox class='flex-fixed color-main ui-height-2 ui-divide-top'></chatbox>
-    <users
-      v-if='showUserList'
-      class='ui-mobile ui-width-5 ui-scroll-y color-main ui-anchor-right ui-shadow'
-      :users='users'>
-    </users>
+  <div class='color-dark flex-column'>
+    <template v-if='chat != null'>
+      <description class='flex-fixed color-main ui-height-2 ui-desktop'>
+        {{chat.description}}
+      </description>
+      <div class='flex-grow flex flex-align-stretch ui-divide-top'>
+        <messages
+          class='flex-grow ui-scroll-y'
+          :messages='chat.messages'>
+        </messages>
+        <users
+          class='ui-desktop flex-fixed ui-width-5 color-main ui-scroll-y ui-divide-left'
+          :users='chat.characters'>
+        </users>
+      </div>
+      <chatbox class='flex-fixed color-main ui-height-2 ui-divide-top'></chatbox>
+    </template>
   </div>
 </template>
 
@@ -27,16 +24,21 @@ import Description from './RoomDescription.vue'
 import Users from './UserList.vue'
 import Messages from './MessageList.vue'
 import Chatbox from './Chatbox.vue'
+import store from '../store'
 
 export default {
   components: {Description, Users, Messages, Chatbox},
 
   data () {
     return {
-      users: [],
-      messages: [],
-      description: 'info here',
-      showUserList: false
+      store,
+      currentChatIndex: 0
+    }
+  },
+
+  computed: {
+    chat () {
+      return this.store.chats[this.currentChatIndex]
     }
   }
 }
