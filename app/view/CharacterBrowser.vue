ca395d7ca0abf77c19ab66e53de23c0de65e5fbc
Kingdaro <kingdaro@gmail.com>
2016-07-29 08:45:13 -0400

    finalized style for character browser

--- app/view/CharacterBrowser.vue
+++ app/view/CharacterBrowser.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class='flex-column flex-align-stretch flex-justify-center ui-overlay'>
+  <div class='flex-column flex-align-stretch ui-overlay'>
     <div class='flex-fixed flex flex-justify-center'>
       <a href='#' class='ui-link ui-margin-1 ui-faded'><h2>Friends</h2></a>
       <a href='#' class='ui-link ui-margin-1'><h2>Looking</h2></a>
       <a href='#' class='ui-link ui-margin-1 ui-faded'><h2>All</h2></a>
     </div>
     <div class='flex-grow flex flex-justify-center flex-wrap ui-scroll-y'>
-      <div v-for='char in looking' class='flex ui-width-8 ui-shade' style='height: 100px; margin: 0.5rem'>
-        <img class='ui-block ui-pointer' :src='getAvatarURL(char.name)' />
-        <div class='flex-grow flex-column'>
-          <div class='flex-fixed ui-padding-2'>
-            <h3<a href='#' class='ui-link'>{{char.name}}</a></h3>
-          </div>
-          <em class='flex-grow ui-block ui-padding-2 ui-small ui-scroll-y'>
+      <a href='#' v-for='char in looking' class='flex ui-width-8 ui-shade ui-link ui-fit-viewport' style='height: 100px; margin: 0.5rem'>
+        <img class='flex-fixed ui-block' :src='getAvatarURL(char.name)' />
+        <div class='flex-grow flex-column' style='width: calc(100% - 100px); overflow-wrap: break-word'>
+          <h3 class='flex-fixed ui-block ui-padding-2'>
+            {{char.name}}
+          </h3>
+          <em class='flex-grow ui-block ui-small ui-padding-0' style='overflow-y: hidden'>
             <span class='status-color' :class='char.status'>{{char.status}}</span>
             <span v-html="' - ' + char.statusmsg" v-if='char.statusmsg'></span>
           </em>
         </div>
-      </div>
+      </a>
     </div>
   </div>
 </template>
@@ -51,6 +51,11 @@ export default {
 
   data () {
     return {
+      groups: [
+        { title: 'Friends', characters: [] },
+        { title: 'Looking', characters: [] },
+        { title: 'All', characters: [] }
+      ],
       store,
       getAvatarURL,
       getProfileURL
