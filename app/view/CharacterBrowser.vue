542e124097f8664ad41b7372de936d9fd569785a
Kingdaro <kingdaro@gmail.com>
2016-07-29 10:05:34 -0400

    split off character chards in a separate component

--- app/view/CharacterBrowser.vue
+++ app/view/CharacterBrowser.vue
@@ -1,5 +1,6 @@
 <template>
   <div class='flex-column flex-align-center ui-overlay'>
+    <back-button align='top' @click.native='close'></back-button>
     <div class='flex-fixed flex flex-justify-center'>
       <a href='#' v-for='(group, index) in groups'
         class='ui-link ui-header-2 ui-margin-1' :class="{ 'ui-faded': currentGroup !== index }"
@@ -12,18 +13,7 @@
       <a href='#' class='ui-link ui-margin-1 ui-faded'><h2>All</h2></a> -->
     </div>
     <div class='flex-grow flex flex-justify-center flex-wrap ui-scroll-y' style='align-content: flex-start'>
-      <a href='#' v-for='char in filteredCharacters' class='flex ui-width-8 ui-shade ui-link ui-fit-viewport' style='height: 100px; margin: 0.5rem'>
-        <img class='flex-fixed ui-block' :src='getAvatarURL(char.name)' />
-        <div class='flex-grow flex-column' style='width: calc(100% - 100px); overflow-wrap: break-word'>
-          <h3 class='flex-fixed ui-block ui-padding-2'>
-            {{ char.name }}
-          </h3>
-          <em class='flex-grow ui-block ui-small ui-padding-0' style='overflow-y: hidden'>
-            <span class='status-color' :class='char.status'>{{ char.status }}</span>
-            <span v-html="' - ' + char.statusmsg" v-if='char.statusmsg'></span>
-          </em>
-        </div>
-      </a>
+      <card v-for='char in filteredCharacters' :character='char'></card>
     </div>
     <div class='flex-fixed ui-width-12 ui-header-2 ui-margin-1 ui-input-icon-left ui-fit-viewport' style='background-color: transparent'>
       <i class='ui-icon mdi mdi-magnify'></i>
@@ -33,6 +23,8 @@
 </template>
 
 <script>
+import Card from './CharacterBrowserCard.vue'
+import BackButton from './BackButton.vue'
 import store from '../store'
 import {getAvatarURL, getProfileURL} from '../f-list'
 import * as util from '../util'
@@ -57,7 +49,7 @@ function sortCharacters (a, b) {
 }
 
 export default {
-  components: {},
+  components: {Card, BackButton},
 
   data () {
     return {
@@ -82,6 +74,10 @@ export default {
           .includes(this.searchText.toLowerCase()))
         .slice(0, 200)
         .sort(sortCharacters)
+    },
+
+    close () {
+      this.store.popOverlay()
     }
   },
 
---
+++ app/view/CharacterBrowserCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <a href='#' class='flex ui-width-8 ui-shade ui-link ui-fit-viewport' style='height: 100px; margin: 0.5rem'>
+    <img class='flex-fixed ui-block' :src='avatarURL' />
+    <div class='flex-grow flex-column' style='width: calc(100% - 100px); overflow-wrap: break-word'>
+      <h3 class='flex-fixed ui-block ui-padding-2'>
+        {{ name }}
+      </h3>
+      <em class='flex-grow ui-block ui-small ui-padding-0' style='overflow-y: hidden'>
+        <span class='status-color' :class='status'>{{ status }}</span>
+        <span v-html="' - ' + statusmsg" v-if='statusmsg'></span>
+      </em>
+    </div>
+  </a>
+</template>
+
+<script>
+import Character from '../models/Character'
+import {getAvatarURL} from '../f-list'
+
+export default {
+  props: {
+    character: Character
+  },
+
+  computed: {
+    name () { return this.character.name },
+    status () { return this.character.status },
+    statusmsg () { return this.character.statusmsg },
+    avatarURL () { return getAvatarURL(this.name) }
+  }
+}
+</script>
