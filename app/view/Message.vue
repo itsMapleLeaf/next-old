df37b5163118d857c2b8013d998495969e2f7089
Kingdaro <kingdaro@gmail.com>
2016-07-30 06:29:52 -0400

    parse /me

--- app/view/Message.vue
+++ app/view/Message.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <character :character='sender' style='margin-right: 0.5rem'></character>
-    <span v-html='message'></span>
+  <div :style='style'>
+    <character :character='sender' style='margin-right: 0.3rem'></character>
+    <span v-html='parsedMessage'></span>
   </div>
 </template>
 
@@ -9,11 +9,26 @@
 import Character from './Character.vue'
 import CharacterModel from '../models/Character'
 
+const meCommand = /^\/me/
+
 export default {
   components: {Character},
+
   props: {
     sender: CharacterModel,
     message: String
+  },
+
+  computed: {
+    parsedMessage () {
+      return this.message.replace(meCommand, '')
+    },
+
+    style () {
+      return {
+        fontStyle: meCommand.test(this.message) ? 'italic' : 'initial'
+      }
+    }
   }
 }
 </script>
