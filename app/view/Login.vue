1d71699b22af531e1e214071a1b43e9ba2f9d959
Kingdaro <kingdaro@gmail.com>
2016-08-01 15:38:48 -0400

    layout fixes

--- app/view/Login.vue
+++ app/view/Login.vue
@@ -4,20 +4,17 @@
     h2.ui-margin-1 Hello, gorgeous.
     form.ui-width-6(@submit.prevent='submit')
       .ui-margin-1.ui-input-icon-left
-        i.ui-icon.mdi.mdi-account-circle
         input(type='text', placeholder='Username', v-model='username')
+        i.ui-icon.mdi.mdi-account-circle
       .ui-margin-1.ui-input-icon-left
-        i.ui-icon.mdi.mdi-lock
         input(type='password', placeholder='••••••••', v-model='password')
+        i.ui-icon.mdi.mdi-lock
       .ui-margin-1
         checkbox(v-model='remember') Remember me
       .ui-margin-1
         button.ui-button(action='submit') Go
-    .ui-margin-1
-      | {{status}}
+    .ui-margin-1 {{ status }}
 </template>
-
-<style></style>
 
 <script>
 import Checkbox from './Checkbox.vue'
