6b6c4eaa39680d991552bcd4c6aca931a8789575
Kingdaro <kingdaro@gmail.com>
2016-08-07 18:56:09 -0400

    moved message filters to the channel room view
    because it doesn't make much sense for it to be on private messages

--- app/view/ChannelRoomView.vue
+++ app/view/ChannelRoomView.vue
@@ -1,9 +1,18 @@
 <template lang="jade">
 .flex-column
+  // room description
   .flex-fixed.color-main.ui-padding-2.ui-scroll-y.res.res-desktop(style='height: 4rem', v-html='room.description')
   .flex-grow.flex.flex-align-stretch.ui-divide-top.ui-divide-bottom
-    messages.flex-grow(:messages='room.messages')
+    .flex-grow.flex-column
+      // chat messages
+      messages.flex-grow(:messages='filteredMessages')
+      // filters
+      .flex-fixed.color-darker.ui-padding-1
+        checkbox(v-for='filter of filters', v-model='filter.enabled', style='margin-right: 0.7rem')
+          | {{ filter.label }}
+    // character list
     users.flex-fixed.ui-width-6.color-main.ui-scroll-y.ui-divide-left.res.res-desktop(:users='room.characters', :ops='room.ops')
+  // chatbox
   chatbox.flex-fixed.color-main.ui-block.ui-padding-2(style='height: 4rem', @message-sent='messageSent')
 </template>
 
@@ -11,14 +20,44 @@
 import Messages from './MessageList.vue'
 import Users from './UserList.vue'
 import Chatbox from './Chatbox.vue'
+import Checkbox from './Checkbox.vue'
 import ChannelRoom from '../models/ChannelRoom'
 import {state, store} from '../store'
 
 export default {
-  components: {Messages, Users, Chatbox},
+  components: {Messages, Users, Chatbox, Checkbox},
 
   data () {
-    return { state }
+    return {
+      state,
+      filters: [
+        {
+          label: 'Chat',
+          enabled: true,
+          filter: msg => msg.type === 'chat'
+        },
+        {
+          label: 'LFRP',
+          enabled: true,
+          filter: msg => msg.type === 'lfrp'
+        },
+        {
+          label: 'Friends',
+          enabled: true,
+          filter: msg => msg.sender.isFriend || msg.sender.isBookmark
+        },
+        {
+          label: 'Admin',
+          enabled: true,
+          filter: msg => msg.type === 'admin'
+        },
+        {
+          label: 'Self',
+          enabled: true,
+          filter: msg => msg.type === 'self'
+        }
+      ]
+    }
   },
 
   props: {
@@ -29,6 +68,15 @@ export default {
     messageSent (message) {
       store.sendChannelMessage(this.room.id, message)
       store.addChannelMessage(this.room.id, this.state.identity, message, 'self')
+    }
+  },
+
+  computed: {
+    filteredMessages () {
+      // lol
+      const findEnabledFilter = msg => filter => filter.enabled && filter.filter(msg)
+      const isMessageFiltered = msg => this.filters.some(findEnabledFilter(msg))
+      return this.room.messages.filter(isMessageFiltered)
     }
   }
 }
--- app/view/MessageList.vue
+++ app/view/MessageList.vue
@@ -1,62 +1,15 @@
 <template lang="jade">
-.flex-column
-  .flex-grow.ui-scroll-y
-    message.ui-padding-1(v-for='msg in filteredMessages', :key='msg.id', :message='msg')
-  .flex-fixed.color-darker.ui-padding-1
-    checkbox(v-for='filter of filters', v-model='filter.enabled', style='margin-right: 0.7rem')
-      | {{ filter.label }}
+div
+  message.ui-padding-1(v-for='msg in messages', :key='msg.id', :message='msg')
 </template>
 
 <script>
 import Message from './Message.vue'
-import Checkbox from './Checkbox.vue'
 
 export default {
-  components: {Message, Checkbox},
-
+  components: {Message},
   props: {
     messages: Array
-  },
-  
-  data () {
-    return {
-      filters: [
-        {
-          label: 'Chat',
-          enabled: true,
-          filter: msg => msg.type === 'chat'
-        },
-        {
-          label: 'LFRP',
-          enabled: true,
-          filter: msg => msg.type === 'lfrp'
-        },
-        {
-          label: 'Friends',
-          enabled: true,
-          filter: msg => msg.sender.isFriend || msg.sender.isBookmark
-        },
-        {
-          label: 'Admin',
-          enabled: true,
-          filter: msg => msg.type === 'admin'
-        },
-        {
-          label: 'Self',
-          enabled: true,
-          filter: msg => msg.type === 'self'
-        }
-      ]
-    }
-  },
-
-  computed: {
-    filteredMessages () {
-      // lol
-      const findEnabledFilter = msg => filter => filter.enabled && filter.filter(msg)
-      const isMessageFiltered = msg => this.filters.some(findEnabledFilter(msg))
-      return this.messages.filter(isMessageFiltered)
-    }
   }
 }
 </script>
