f89f5cf27a0e8c1291dc49d2a208c7e5331318df
Kingdaro <kingdaro@gmail.com>
2016-08-07 18:44:57 -0400

    add label class on private room titles

--- app/view/PrivateRoomTitle.vue
+++ app/view/PrivateRoomTitle.vue
@@ -1,7 +1,7 @@
 <template lang="jade">
 span
   img(:src='room.partner.avatarURL', style='height: 1em; position: relative; top: 0.1em')
-  | {{ room.partner.name }}
+  span.ui-label {{ room.partner.name }}
 </template>
 
 <script>
