95c965bed1025420fdf30a4f114994de6df50dd4
Kingdaro <kingdaro@gmail.com>
2016-08-07 18:39:51 -0400

    some more helpful message properties

--- app/models/Message.js
+++ app/models/Message.js
@@ -2,9 +2,11 @@ import parseBBC from '../parse-bbc'
 
 export default class Message {
   constructor (sender, message, type) {
-    this.sender = sender   // Character
-    this.message = parseBBC(message) // String
+    this.sender = sender   // Character object
+    this.originalMessage = message
+    this.message = parseBBC(message)
     this.time = Date.now()
     this.type = type // either 'chat', 'lfrp', 'self', or 'admin'
+    this.id = Math.random() * 999999
   }
 }
