0cdfc977c08573f1be5de0b7d6d74716d7b92a57
Kingdaro <kingdaro@gmail.com>
2016-08-07 15:11:41 -0400

    using jade mixins to make things neater

--- app/view/ChannelSelect.vue
+++ app/view/ChannelSelect.vue
@@ -1,34 +1,38 @@
 <template lang="jade">
+mixin channel-list
+  a.ui-block.ui-padding-3(href='#', v-for='channel in channels', :class="{ 'highlight-green': store.isChannelJoined(channel.id) }", @click='toggleChannel(channel.id)')
+    .flex.flex-justify-space-between
+      span(v-html='channel.name')
+      span {{channel.users}}
+    em.ui-text-small.ui-text-faded(v-if='channel.id !== channel.name')
+      | {{channel.id}}
+
+mixin search-box
+  .ui-input-icon-left
+    i.ui-icon.mdi.mdi-magnify
+    input(type='text', v-model='searchText', placeholder='Search...')
+
+mixin show-all-channels
+  checkbox(v-model='showAll') Show ALL channels (lag warning)
+
 .ui-overlay(@click.self='close')
   form.ui-panel.ui-height-12.ui-fit-viewport.flex-column.res.res-desktop.res-mobile-portrait(@submit.prevent='')
     .flex-grow.color-dark.ui-width-9.ui-fit-width.ui-margin-bottom-1.ui-scroll-y
-      a.ui-block.ui-padding-3(href='#', v-for='channel in channels', :class="{ 'highlight-green': store.isChannelJoined(channel.id) }", @click='toggleChannel(channel.id)')
-        .flex.flex-justify-space-between
-          span(v-html='channel.name')
-          span {{channel.users}}
-        em.ui-text-small.ui-text-faded(v-if='channel.id !== channel.name')
-          | {{channel.id}}
-    .flex-fixed.ui-field.ui-input-icon-left.ui-block-center
-      i.ui-icon.mdi.mdi-magnify
-      input(type='text', v-model='searchText', placeholder='Search...')
+      +channel-list
+    .flex-fixed.ui-field.ui-block-center
+      +search-box
     .flex-fixed.ui-field.ui-text-center
-      checkbox(v-model='showAll') Show ALL channels (lag warning)
+      +show-all-channels
     back-button(@click.native='close')
+
   form.flex.ui-panel.ui-fit-viewport.flex.res.res-mobile-landscape(@submit.prevent='')
     .flex-grow.color-dark.ui-width-8.ui-scroll-y.ui-fit-height.ui-margin-left-2.ui-margin-right-2
-      a.ui-block.ui-padding-3(href='#', v-for='channel in channels', :class="{ 'highlight-green': store.isChannelJoined(channel.id) }", @click='toggleChannel(channel.id)')
-        .flex.flex-justify-space-between
-          span(v-html='channel.name')
-          span {{channel.users}}
-        em.ui-text-small.ui-text-faded(v-if='channel.id !== channel.name')
-          | {{channel.id}}
+      +channel-list
     .fiex-fixed.ui-margin-right-2.flex-column.flex-center
       .ui-field
-        .ui-input-icon-left
-          i.ui-icon.mdi.mdi-magnify
-          input(type='text', v-model='searchText', placeholder='Search...')
+        +search-box
       .ui-field
-        checkbox(v-model='showAll') Show ALL channels (lag warning)
+        +show-all-channels
     back-button(align='middle', @click.native='close')
 </template>
 
