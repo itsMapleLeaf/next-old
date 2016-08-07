aa019c7c6279f4dc57a1fd22ce0e05191684de33
Kingdaro <kingdaro@gmail.com>
2016-08-07 18:18:05 -0400

    remove checkbox padding

--- app/view/Checkbox.vue
+++ app/view/Checkbox.vue
@@ -1,5 +1,5 @@
 <template lang="jade">
-a.ui-focus-darken.ui-padding-1(:class="{'ui-faded': !value}", href='#', @keydown.space="$emit('input', !value)", @keydown.enter.prevent='', @click="$emit('input', !value)")
+a.ui-focus-darken(:class="{'ui-faded': !value}", href='#', @keydown.space="$emit('input', !value)", @keydown.enter.prevent='', @click="$emit('input', !value)")
   small
     i.mdi(:class="'mdi-' + (value ? 'checkbox-marked' : 'checkbox-blank-outline')")
     span
