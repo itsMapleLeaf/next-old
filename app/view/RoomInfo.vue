b00b0f2086fd2041afb753e0510ab19878fad2e7
Kingdaro <kingdaro@gmail.com>
2016-08-08 15:58:56 -0400

    fix room info component

--- app/view/RoomInfo.vue
+++ app/view/RoomInfo.vue
@@ -1,17 +1,17 @@
 <template lang="jade">
-.ui-overlay(@click.self='store.popOverlay')
+.ui-overlay(@click.self='popOverlay')
   .ui-panel.ui-scroll-y
     .ui-padding-subtle.ui-text-center
-      h2 {{ title }}
-    .color-dark.ui-padding-square-1.ui-fit-width.ui-pre-wrap(style='width: 40em', v-html='description')
-    user-list.color-dark.ui-margin-1.ui-width-7.ui-fit-width.ui-block-center.res.res-mobile(:users='users')
-    back-button(align='top', @click.native='store.popOverlay')
+      h2 {{ room.name }}
+    .color-dark.ui-padding-square-1.ui-fit-width.ui-pre-wrap(style='width: 40em', v-html='room.description')
+    user-list.color-dark.ui-margin-1.ui-width-7.ui-fit-width.ui-block-center.res.res-mobile(:users='room.characters', :ops='room.ops')
+    back-button(align='top', @click.native='popOverlay')
 </template>
 
 <script>
 import UserList from './UserList.vue'
 import BackButton from './BackButton.vue'
-import {state} from '../store'
+import {store, state} from '../store'
 
 export default {
   components: {UserList, BackButton},
@@ -22,9 +22,11 @@ export default {
   },
 
   computed: {
-    title () { return this.state.currentRoom.name },
-    description () { return this.state.currentRoom.description },
-    users () { return this.state.currentRoom.characters }
+    room () { return this.state.currentRoom },
+  },
+
+  methods: {
+    popOverlay () { store.popOverlay() }
   }
 }
 </script>
