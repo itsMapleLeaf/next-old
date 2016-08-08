bb1d78be72e573f0744bbcfa7fd1acbec0073275
Kingdaro <kingdaro@gmail.com>
2016-08-08 17:01:41 -0400

    split off directives into a separate file

---
+++ app/directives.js
@@ -0,0 +1,24 @@
+function scrollToBottom (event) {
+  const el = event.target
+  el.dataset.bottomScrollEnabled = el.scrollHeight - el.scrollTop === el.clientHeight
+}
+
+const bottomScroll = {
+  bind (el, binding) {
+    el.dataset.bottomScrollEnabled = 'true'
+    el.addEventListener('scroll', scrollToBottom)
+  },
+  update (el, binding) {
+    console.log('update', el.dataset.bottomScrollEnabled)
+    if (el.dataset.bottomScrollEnabled === 'true') {
+      Vue.nextTick(() => {
+        el.scrollTop = el.scrollHeight
+      })
+    }
+  },
+  unbind (el, binding) {
+    el.removeEventListener('scroll', scrollToBottom)
+  }
+}
+
+export { bottomScroll }
--- app/view/MessageList.vue
+++ app/view/MessageList.vue
@@ -6,38 +6,15 @@ div(v-bottom-scroll='')
 <script>
 import Message from './Message.vue'
 import Vue from 'vue'
-
-function onScroll (event) {
-  const el = event.target
-  el.dataset.bottomScrollEnabled = el.scrollHeight - el.scrollTop === el.clientHeight
-  console.log('scroll', el.dataset.bottomScrollEnabled)
-}
+import {bottomScroll} from '../directives'
 
 export default {
   components: {Message},
-
   props: {
     messages: Array
   },
-
   directives: {
-    bottomScroll: {
-      bind (el, binding) {
-        el.dataset.bottomScrollEnabled = 'true'
-        el.addEventListener('scroll', onScroll)
-      },
-      update (el, binding) {
-        console.log('update', el.dataset.bottomScrollEnabled)
-        if (el.dataset.bottomScrollEnabled === 'true') {
-          Vue.nextTick(() => {
-            el.scrollTop = el.scrollHeight
-          })
-        }
-      },
-      unbind (el, binding) {
-        el.removeEventListener('scroll', onScroll)
-      }
-    }
+    bottomScroll
   }
 }
 </script>
