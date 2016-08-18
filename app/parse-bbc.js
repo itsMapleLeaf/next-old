122fb38a058e7c791594ed923b75231d60b79fdb
Kingdaro <kingdaro@gmail.com>
2016-08-18 14:04:31 -0400

    finally indented this trash

--- app/parse-bbc.js
+++ app/parse-bbc.js
@@ -3,6 +3,7 @@ import urllib from 'url'
 import path from 'path'
 
 const regexp = /\[(\w+?)=?([^\]]*)\]([\s\S]+?)\[\/\1\]|(https?:\/\/[^\[\]\(\)\s]+)/gi // lol
+const imageExtensions = ['.png', '.jpg', '.jpeg', '.svg', '.gif']
 
 export default function parseBBC (input) {
   return input.replace(regexp, (match, tag, value, text, url) => {
@@ -23,24 +24,37 @@ export default function parseBBC (input) {
       case 'sup': return `<sup>${text}</sup>`
       case 'sub': return `<small>${text}</small>`
 
-      case 'color': return `<span class="chat-color ${value}">${text}</span>`
+      case 'color':
+        return `<span class="chat-color ${value}">${text}</span>`
 
       case 'url':
         const href = value || text
         const {hostname, pathname} = urllib.parse(href)
         const ext = path.extname(pathname)
-        const icon = ['.png', '.jpg', '.jpeg', '.svg', '.gif'].includes(ext) ? 'image' : 'link-variant'
-        return `<a class="ui-link" href="${href}" target="_blank" title="${hostname}"><i class='mdi mdi-${icon}'></i> ${text}</a>`
+        const icon = imageExtensions.includes(ext) ? 'image' : 'link-variant'
+        return `
+          <a class="ui-link" href="${href}" target="_blank" title="${hostname}">
+            <i class='mdi mdi-${icon}'></i> ${text}
+          </a>`
 
       case 'channel':
-        return `<a href='#' class='ui-link' data-join-channel='${text}'><i class='mdi mdi-earth'></i> ${text}</a>`
+        return `
+          <a href='#' class='ui-link' data-join-channel='${text}'>
+            <i class='mdi mdi-earth'></i> ${text}
+          </a>`
 
       case 'session':
-        return `<a href='#' class='ui-link' data-join-channel='${text}'><i class='mdi mdi-key-variant'></i> ${value}</a>`
+        return `
+          <a href='#' class='ui-link' data-join-channel='${text}'>
+            <i class='mdi mdi-key-variant'></i> ${value}
+          </a>`
 
       case 'icon':
       case 'user':
-        return `<a href="${getProfileURL(text)}" class="ui-link"><img src="${getAvatarURL(text)}" style="width: 50px; height: auto" /></a>`
+        return `
+          <a href="${getProfileURL(text)}" class="ui-link">
+            <img src="${getAvatarURL(text)}" style="width: 50px; height: auto" />
+          </a>`
 
       default:
         return match
