438129872ed3a67e3f6af9916be91feb52fcbbe4
Kingdaro <kingdaro@gmail.com>
2016-08-19 11:48:42 -0400

    rewrite the socket as an es6 module

--- app/socket.js
+++ app/socket.js
@@ -1,136 +1,134 @@
 import {store, state} from './store'
 
-export default {
-  ws: null,
+let ws = null
 
-  connect () {
-    this.ws = new window.WebSocket('wss://chat.f-list.net:9799')
-    return this.ws
+export function connect () {
+  ws = new window.WebSocket('wss://chat.f-list.net:9799')
+  return ws
+}
+
+export function disconnect () {
+  ws.close()
+}
+
+export function sendCommand (command, params) {
+  const data = params ? `${command} ${JSON.stringify(params)}` : command
+  ws.send(data)
+  console.log(`Sent command: ${data}`)
+}
+
+const serverCommands = {
+  IDN () {
+    console.info('Successfully identified with server.')
+    store.setSocketState('identified')
   },
 
-  disconnect () {
-    this.ws.close()
+  HLO (params) { console.info(params.message) },
+  CON (params) { console.info(`There are ${params.count} characters online.`) },
+
+  /* ping~! */
+  PIN () {
+    /* pong~! */
+    sendCommand('PIN')
   },
 
-  sendCommand (command, params) {
-    const data = params ? `${command} ${JSON.stringify(params)}` : command
-    this.ws.send(data)
-    console.log(`Sent command: ${data}`)
-  },
+  // ignored we get friends from login
+  FRL () {},
 
-  handleCommand (command, params) {
-    const handlers = {
-      IDN () {
-        console.info('Successfully identified with server.')
-        store.setSocketState('identified')
-      },
+  IGN (params) {
+    switch (params.action) {
+      case 'init':
+      case 'list':
+        store.setIgnoreList(params.characters)
+        break
 
-      HLO () { console.info(params.message) },
-      CON () { console.info(`There are ${params.count} characters online.`) },
+      case 'add':
+        store.addIgnored(params.character)
+        break
 
-      /* ping~! */
-      PIN () {
-        /* pong~! */
-        this.sendCommand('PIN')
-      },
+      case 'delete':
+        store.removeIgnored(params.character)
+        break
 
-      // ignored, we get friends from login
-      FRL () {},
-
-      IGN () {
-        switch (params.action) {
-          case 'init':
-          case 'list':
-            store.setIgnoreList(params.characters)
-            break
-
-          case 'add':
-            store.addIgnored(params.character)
-            break
-
-          case 'delete':
-            store.removeIgnored(params.character)
-            break
-
-          default:
-            console.warn(`Unknown ignore action "${params.action}"`, params)
-        }
-      },
-
-      LIS () { store.addCharacterBatch(params.characters) },
-      ADL () { store.setAdminList(params.ops) },
-      NLN () { store.addCharacter(params.identity, params.gender) },
-      FLN () { store.removeCharacter(params.character) },
-      STA () { store.setCharacterStatus(params.character, params.status, params.statusmsg) },
-
-      // public channel list
-      CHA () {
-        const channels = params.channels.map(ch => ({ id: ch.name, name: ch.name, users: ch.characters }))
-        store.addChannels(channels)
-      },
-
-      // private channel list
-      ORS () {
-        const channels = params.channels.map(ch => ({ id: ch.name, name: ch.title, users: ch.characters }))
-        store.addChannels(channels)
-      },
-
-      // someone joined a channel
-      // if it's us, add a new chat
-      JCH () {
-        if (params.character.identity === state.identity) {
-          store.addChannelChat(params.channel, params.title)
-        } else {
-          store.addChannelCharacter(params.channel, params.character.identity)
-        }
-      },
-
-      // someone left a channel
-      // if it's us, remove that channel
-      LCH () {
-        store.removeChannelCharacter(params.channel, params.character)
-        if (params.character === state.identity) {
-          store.removeChannelChat(params.channel)
-        }
-      },
-
-      // list of ops for a channel
-      COL () {
-        store.setChannelOps(params.channel, params.oplist)
-      },
-
-      // initial channel information
-      ICH () {
-        const names = params.users.map(user => user.identity)
-        store.setChannelCharacters(params.channel, names)
-        store.setChannelMode(params.channel, params.mode)
-      },
-
-      // channel description update
-      CDS () {
-        store.setChannelDescription(params.channel, params.description)
-      },
-
-      // channel message
-      MSG () {
-        store.addChannelMessage(params.channel, params.character, params.message, 'chat')
-      },
-
-      // LFRP message
-      LRP () {
-        store.addChannelMessage(params.channel, params.character, params.message, 'lfrp')
-      },
-
-      // private message
-      PRI () {
-        store.addPrivateMessage(params.character, params.character, params.message, 'chat')
-      },
-
-      VAR () {}
+      default:
+        console.warn(`Unknown ignore action "${params.action}"`, params)
     }
+  },
 
-    handlers[command]
-      ? handlers[command].call(this)
-      : console.warn(`Unknown command: ${command} ${JSON.stringify(params)}`)
-  }
+  LIS (params) { store.addCharacterBatch(params.characters) },
+  ADL (params) { store.setAdminList(params.ops) },
+  NLN (params) { store.addCharacter(params.identity, params.gender) },
+  FLN (params) { store.removeCharacter(params.character) },
+  STA (params) { store.setCharacterStatus(params.character, params.status, params.statusmsg) },
+
+  // public channel list
+  CHA (params) {
+    const channels = params.channels.map(ch => ({ id: ch.name, name: ch.name, users: ch.characters }))
+    store.addChannels(channels)
+  },
+
+  // private channel list
+  ORS (params) {
+    const channels = params.channels.map(ch => ({ id: ch.name, name: ch.title, users: ch.characters }))
+    store.addChannels(channels)
+  },
+
+  // someone joined a channel
+  // if it's us add a new chat
+  JCH (params) {
+    if (params.character.identity === state.identity) {
+      store.addChannelChat(params.channel, params.title)
+    } else {
+      store.addChannelCharacter(params.channel, params.character.identity)
+    }
+  },
+
+  // someone left a channel
+  // if it's us remove that channel
+  LCH (params) {
+    store.removeChannelCharacter(params.channel, params.character)
+    if (params.character === state.identity) {
+      store.removeChannelChat(params.channel)
+    }
+  },
+
+  // list of ops for a channel
+  COL (params) {
+    store.setChannelOps(params.channel, params.oplist)
+  },
+
+  // initial channel information
+  ICH (params) {
+    const names = params.users.map(user => user.identity)
+    store.setChannelCharacters(params.channel, names)
+    store.setChannelMode(params.channel, params.mode)
+  },
+
+  // channel description update
+  CDS (params) {
+    store.setChannelDescription(params.channel, params.description)
+  },
+
+  // channel message
+  MSG (params) {
+    store.addChannelMessage(params.channel, params.character, params.message, 'chat')
+  },
+
+  // LFRP message
+  LRP (params) {
+    store.addChannelMessage(params.channel, params.character, params.message, 'lfrp')
+  },
+
+  // private message
+  PRI (params) {
+    store.addPrivateMessage(params.character, params.character, params.message, 'chat')
+  },
+
+  VAR () {}
+}
+
+export function handleCommand (command, params) {
+  serverCommands[command]
+    ? serverCommands[command](params)
+    : console.warn(`Unknown command: ${command} ${JSON.stringify(params)}`)
 }
