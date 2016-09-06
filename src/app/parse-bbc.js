1e94b01a98d82c3464debeeaaa437a333da45035
Kingdaro <kingdaro@gmail.com>
2016-09-06 04:42:09 -0400

    cleaned up BBC parser

--- src/app/parse-bbc.js
+++ src/app/parse-bbc.js
@@ -2,17 +2,17 @@ import {getProfileURL, getAvatarURL} from './f-list'
 import urllib from 'url'
 import path from 'path'
 
-const regexp = /\[(\w+?)=?([^\]]*)\]([\s\S]+?)\[\/\1\]|(https?:\/\/[^\[\]\(\)\s]+)/gi // lol
+const bbcExpression = /\[(\w+?)=?([^\]]*)\]([\s\S]+?)\[\/\1\]|(https?:\/\/[^\[\]\(\)\s]+)/gi // lol
 const imageExtensions = ['.png', '.jpg', '.jpeg', '.svg', '.gif']
 
-export default function parseBBC (input) {
-  return input.replace(regexp, (match, tag, value, text, url) => {
+function parseBBC (input) {
+  return input.replace(bbcExpression, (match, tag, value, text, url) => {
     if (url) {
       tag = 'url'
       value = text = url
     } else if (tag === 'noparse') {
       return text
-    } else if (tag !== 'url' && regexp.test(text)) {
+    } else if (tag !== 'url') {
       text = parseBBC(text)
     }
 
@@ -23,37 +23,58 @@ export default function parseBBC (input) {
       case 's': return `<del>${text}</del>`
       case 'sup': return `<sup>${text}</sup>`
       case 'sub': return `<small>${text}</small>`
-
-      case 'color':
-        return `<span class="chat-color ${value}">${text}</span>`
-
-      case 'url':
-        const href = value || text
-        const {hostname, pathname} = urllib.parse(href)
-        const ext = path.extname(pathname)
-        const icon = imageExtensions.includes(ext) ? 'image' : 'link-variant'
-        return `<a class="ui-link" href="${href}" target="_blank" title="${hostname}">` +
-          `<i class='mdi mdi-${icon}'></i> ${text}` +
-          `</a>`
-
-      case 'channel':
-        return `<a href='#' class='ui-link' data-join-channel='${text}'>` +
-          `<i class='mdi mdi-earth'></i> ${text}` +
-          `</a>`
-
-      case 'session':
-        return `<a href='#' class='ui-link' data-join-channel='${text}'>` +
-          `<i class='mdi mdi-key-variant'></i> ${value}` +
-          `</a>`
+      case 'color': return formatColor(value, text)
+      case 'url': return formatURL(value, text)
+      case 'channel': return formatPublicChannelLink(text)
+      case 'session': return formatPrivateChannelLink(text, value)
 
       case 'icon':
       case 'user':
-        return `<a href="${getProfileURL(text)}" class="ui-link">` +
-          `<img src="${getAvatarURL(text)}" style="width: 50px; height: auto" />` +
-          `</a>`
+      case 'eicon': return formatUserIcon(text)
 
-      default:
-        return match
+      default: return match
     }
   })
 }
+
+function formatColor (color, text) {
+  return `<span class="chat-color ${color}">${text}</span>`
+}
+
+function formatURL (url, text) {
+  const href = url || text
+  const {hostname, pathname} = urllib.parse(href)
+  const ext = path.extname(pathname)
+  const icon = imageExtensions.includes(ext) ? 'image' : 'link-variant'
+  return (
+    `<a class="ui-link" href="${href}" target="_blank" title="${hostname}">` +
+      `<i class='mdi mdi-${icon}'></i> ${text}` +
+    `</a>`
+  )
+}
+
+function formatPublicChannelLink (channel) {
+  return (
+    `<a href='#' class='ui-link' data-join-channel='${channel}'>` +
+      `<i class='mdi mdi-earth'></i> ${channel}` +
+    `</a>`
+  )
+}
+
+function formatPrivateChannelLink (id, name) {
+  return (
+    `<a href='#' class='ui-link' data-join-channel="${id}">` +
+      `<i class='mdi mdi-key-variant'></i> ${name}` +
+    `</a>`
+  )
+}
+
+function formatUserIcon (user) {
+  return (
+    `<a href="${getProfileURL(user)}" class="ui-link">` +
+      `<img src="${getAvatarURL(user)}" style="width: 50px; height: auto" />` +
+    `</a>`
+  )
+}
+
+export default parseBBC
