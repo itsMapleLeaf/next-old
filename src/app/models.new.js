f2e7c24dbebe339ca7fb5834c4a15cb8cf29de3b
Kingdaro <kingdaro@gmail.com>
2016-09-08 09:54:31 -0400

    use type checks

--- src/app/models.new.js
+++ src/app/models.new.js
@@ -1,17 +1,82 @@
-export function Character (name, gender, status = 'online', statusmsg = '') {
+// @flow
+
+// alias types
+type CharacterName = string
+
+// enum types
+type Gender
+  = 'Male'
+  | 'Female'
+  | 'Transgender'
+  | 'Herm'
+  | 'Shemale'
+  | 'Male-herm'
+  | 'None'
+
+type Status
+  = 'online'
+  | 'looking'
+  | 'busy'
+  | 'away'
+  | 'dnd'
+  | 'idle'
+  | 'offline'
+
+type MessageType
+  = 'chat'
+  | 'lfrp'
+  | 'friend'
+  | 'admin'
+  | 'self'
+
+// object types
+export type Character = {
+  name: CharacterName,
+  gender: Gender,
+  status: Status,
+  statusmsg: string
+}
+
+export type Message = {
+  sender: CharacterName,
+  message: string,
+  type: MessageType,
+  time: number
+}
+
+export type Channel = {
+  id: string,
+  name: string,
+  description: string,
+  mode: 'chat' | 'ads' | 'both',
+  characters: CharacterName[],
+  ops: CharacterName[]
+}
+
+export type PrivateChat = {
+  partner: CharacterName,
+  messages: Message[]
+}
+
+// constructors
+export function newCharacter (
+  name: CharacterName,
+  gender: Gender,
+  status: Status = 'online',
+  statusmsg: string = ''
+): Character {
   return { name, gender, status, statusmsg }
 }
 
-export function Message (sender, message, type) {
-  return {
-    sender, // character name
-    type,   // either 'chat', 'lfrp', 'self', 'admin', or 'friend'
-    message,
-    time: Date.now()
-  }
+export function newMessage (
+  sender: CharacterName,
+  message: string,
+  type: MessageType
+): Message {
+  return { sender, type, message, time: Date.now() }
 }
 
-export function Channel (id, name) {
+export function newChannel (id: string, name: string): Channel {
   return {
     id,
     name,
@@ -22,7 +87,7 @@ export function Channel (id, name) {
   }
 }
 
-export function PrivateChat (partner) {
+export function newPrivateChat (partner: CharacterName): PrivateChat {
   return {
     partner,
     messages: []
