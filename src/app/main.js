4b0c649391c3694a3a579962533bbbfc4d11509d
Kingdaro <kingdaro@gmail.com>
2016-09-11 13:33:04 -0400

    main config

--- src/app/main.js
+++ src/app/main.js
@@ -2,16 +2,23 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './components/App.vue'
+import * as store from './store'
 
-import './styles/root.styl'
+// import styles
+// import './styles/root.styl'
 
+// configure keycodes
 Vue.config.keyCodes = {
   enter: 13,
   space: 32
 }
 
+// configure vue resource
 Vue.use(VueResource)
 Vue.http.options.emulateJSON = true
+
+// make store accessible to vue components
+Vue.prototype.$store = store
 
 /* eslint no-new: 0 */
 new Vue({
