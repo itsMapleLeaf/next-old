c34d1a8fec2c3afcacd1e67dd14bbca32aebbd8a
Kingdaro <kingdaro@gmail.com>
2016-09-15 13:05:20 -0400

    set up new store format

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,5 +1,24 @@
-import EventEmitter from 'events'
+type Name = string
 
-const store = new EventEmitter()
+class State {
+  account: string = ''
+  ticket: string = ''
 
-export default store
+  userCharacters: Name[] = []
+  identity: Name = ''
+}
+
+class Store {
+  state = new State()
+  history = []
+
+  set (key, value) {
+    this.state[key] = value
+    this.history.push({ key, value })
+  }
+}
+
+const store = new Store()
+const state = store.state
+
+export { store, state }
