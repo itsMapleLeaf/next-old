9b28921bc9cee7b22da63f9bebb67f1c3ec9e8a6
Kingdaro <kingdaro@gmail.com>
2016-09-15 15:10:28 -0400

    PROMISES!

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -5,18 +5,21 @@ import * as flist from '../lib/f-list'
 
 export const store = {
   init () {
+    this.authenticate()
+  },
+
+  authenticate () {
+    state.loadingMessage = 'Setting things up...'
     storage.getItem('auth').then(auth => {
-      if (!auth) {
-        return Promise.reject()
-      } else {
-        state.loadingMessage = 'Setting things up...'
-        return flist.getCharacters(auth.account, auth.ticket)
-      }
+      return auth ? Promise.resolve(auth) : Promise.reject()
     })
-    .then(characters => {
-      state.userCharacters = characters
+    .then(({ account, ticket }) => {
+      state.account = account
+      state.ticket = ticket
+      return this.openCharacterList(account, ticket)
+    })
+    .then(() => {
       state.loadingMessage = ''
-      state.currentView = 'CharacterList'
     })
     .catch(() => {
       state.loadingMessage = ''
@@ -25,28 +28,40 @@ export const store = {
   },
 
   login (account: string, password: string, remember: boolean): Promise<void> {
+    state.loadingMessage = 'Logging in...'
     return flist.getTicket(account, password).then(ticket => {
+      const data = { account, ticket }
       if (remember) {
-        storage.setItem('auth', { account, ticket })
+        storage.setItem('auth', data)
       } else {
         storage.clear()
       }
+      return data
+    })
+    .then(auth => {
+      return this.openCharacterList(auth.account, auth.ticket)
+    })
+    .then(() => {
+      state.loadingMessage = ''
     })
     .catch(err => {
-      if (err) {
-        return Promise.reject('Wrong username or password.')
-      } else {
-        return Promise.reject(`
-          Could not connect to the F-list website.
-          Either they're doing maintenance,
-          or someone spilled coke on the servers again.
-        `)
-      }
+      return Promise.reject(err || `
+        Could not connect to the F-list website.
+        Either they're doing maintenance,
+        or someone spilled coke on the servers again.
+      `)
     })
   },
 
   setIdentity (name: Name) {
     state.identity = name
     state.currentView = 'Chat'
+  },
+
+  openCharacterList (account: string, ticket: string): Promise<void> {
+    return flist.getCharacters(account, ticket).then(characters => {
+      state.userCharacters = characters
+      state.currentView = 'CharacterList'
+    })
   }
 }
