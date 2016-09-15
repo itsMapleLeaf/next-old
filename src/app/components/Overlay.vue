5b8c73644601c08e8ae4c82df886aeb96dca0bac
Kingdaro <kingdaro@gmail.com>
2016-09-14 21:28:44 -0400

    write up simple overlay component

---
+++ src/app/components/Overlay.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class='overlay-shade'>
+    <div class='overlay'>
+      <div class='overlay-header'>
+        <h2>{{ header }}</h2>
+      </div>
+      <div class='overlay-content'>
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    header: String
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+@require '../stylus/mixins'
+@require '../stylus/colors'
+
+.overlay-shade
+  background: rgba(black, 0.5)
+  fullscreen()
+
+.overlay-header
+  background: darken($theme-color, 20%)
+  accent-border(bottom)
+
+.overlay-content
+  background: $theme-color
+</style>
