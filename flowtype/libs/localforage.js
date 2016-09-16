bf51605b4572a62726b69665c0b2e7ac2bdd9342
Kingdaro <kingdaro@gmail.com>
2016-09-16 14:05:40 -0400

    add function types for localforage

--- flowtype/libs/localforage.js
+++ flowtype/libs/localforage.js
@@ -1,3 +1,5 @@
 declare module 'localforage' {
-  declare var exports: typeof Object
+  declare function getItem (key: string): Promise<any>
+  declare function setItem <T> (key: string, value: T): Promise<T>
+  declare function clear (): Promise<void>
 }
