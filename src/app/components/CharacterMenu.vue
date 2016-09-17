3261fdad4072ff624fbb5e586bf06527e72eb47f
Kingdaro <kingdaro@gmail.com>
2016-09-17 18:02:49 -0400

    added a slide transition

--- src/app/components/CharacterMenu.vue
+++ src/app/components/CharacterMenu.vue
@@ -1,35 +1,36 @@
 <template>
-  <div class='overlay-shade' @click.self="$emit('closed')">
-    <form class='overlay-panel'>
-      <fieldset>
-        <h3>{{ character.name }}</h3>
-        <small :class="'character-gender-' + character.gender.toLowerCase()">
-          {{ character.gender }}
-        </small>
-      </fieldset>
-      <fieldset>
-        <Avatar class='character-menu-avatar' :name='character.name'></Avatar>
-      </fieldset>
-      <fieldset>
-        <div class='character-menu-status'>
-          <Status :status='character.status' :statusmsg='character.statusmsg'></Status>
-        </div>
-      </fieldset>
-      <fieldset v-for='friend in friends[character.name] || []'>
-        <div class='character-menu-friend'>
-          <i class='mdi mdi-heart'></i>
-          {{ friend }}
-        </div>
-      </fieldset>
-    </form>
-  </div>
+  <transition name='overlay-slide-right' appear>
+    <div class='overlay-shade' @click.self="$emit('closed')">
+      <form class='overlay-panel'>
+        <fieldset>
+          <h3>{{ character.name }}</h3>
+          <small :class="'character-gender-' + character.gender.toLowerCase()">
+            {{ character.gender }}
+          </small>
+        </fieldset>
+        <fieldset>
+          <Avatar class='character-menu-avatar' :name='character.name'></Avatar>
+        </fieldset>
+        <fieldset>
+          <div class='character-menu-status'>
+            <Status :status='character.status' :statusmsg='character.statusmsg'></Status>
+          </div>
+        </fieldset>
+        <fieldset v-for='friend in friends[character.name] || []'>
+          <div class='character-menu-friend'>
+            <i class='mdi mdi-heart'></i>
+            {{ friend }}
+          </div>
+        </fieldset>
+      </form>
+    </div>
+  </transition>
 </template>
 
 <script>
 import Avatar from './Avatar.vue'
 import Status from './Status.vue'
 import {getters} from '../store'
-import {parse} from '../lib/bbc'
 
 export default {
   props: {
@@ -40,22 +41,18 @@ export default {
     Status
   },
   computed: {
-    ...getters(['friends']),
-
-    parsedStatus () {
-      const {statusmsg} = this.character
-      return statusmsg ? parse(statusmsg) : ''
-    }
+    ...getters(['friends'])
   }
 }
 </script>
 
 <style lang='stylus' scoped>
-@import '../styles/mixins'
-@import '../styles/colors'
+@require '../styles/mixins'
+@require '../styles/colors'
 
 .overlay-panel
   anchor(top right bottom)
+  position: fixed
   width: 12em
   padding: 0.75em 1em
 
