da3e595e46cdfdde1eda19b3fbe4d61614dfa117
Kingdaro <kingdaro@gmail.com>
2016-09-18 22:34:46 -0400

    character menu options

--- src/app/components/CharacterMenu.vue
+++ src/app/components/CharacterMenu.vue
@@ -1,8 +1,8 @@
 <template>
   <transition name='overlay-slide-right' appear>
     <div class='overlay-shade' @click.self="$emit('closed')">
-      <div class='overlay-panel character-menu-panel'>
-        <form class='character-menu-info'>
+      <div class='overlay-panel'>
+        <form class='info'>
           <fieldset>
             <h3>
               <ProfileLink :name='character.name'></ProfileLink>
@@ -13,23 +13,28 @@
           </fieldset>
           <fieldset>
             <ProfileLink :name='character.name'>
-              <Avatar class='character-menu-avatar' :name='character.name'></Avatar>
+              <Avatar :name='character.name'></Avatar>
             </ProfileLink>
           </fieldset>
           <fieldset>
-            <div class='character-menu-status'>
+            <div class='status'>
               <Status :status='character.status' :statusmsg='character.statusmsg'></Status>
             </div>
           </fieldset>
           <fieldset v-for='friend in friends[character.name] || []'>
             <ProfileLink :name='friend'>
-              <div class='character-menu-friend'>
+              <div class='friend'>
                 <i class='mdi mdi-heart'></i>
                 {{ friend }}
               </div>
             </ProfileLink>
           </fieldset>
         </form>
+        <nav>
+          <a href='#' v-for='opt in menuOptions' @click='opt.action && opt.action()'>
+            <i :class="'mdi mdi-' + opt.icon"></i> {{ opt.label }}
+          </a>
+        </nav>
       </div>
     </div>
   </transition>
@@ -52,6 +57,14 @@ export default {
   },
   computed: {
     ...getters(['friends']),
+    menuOptions () {
+      return [
+        { label: 'Send Message', icon: 'comment' },
+        { label: 'Bookmark', icon: 'bookmark-outline' },
+        { label: 'Ignore', icon: 'minus-circle-outline' },
+        { label: 'View Profile', icon: 'link-variant' }
+      ]
+    }
   }
 }
 </script>
@@ -64,23 +77,23 @@ export default {
 @require 'mixins/highlight'
 @require 'transitions/overlay'
 
-.character-menu-panel
-  background: darken($theme-color, 20%)
+.overlay-panel
+  background: theme-darker(20%)
   anchor(top right bottom)
   position: fixed
   width: 12em
 
-.character-menu-info
+.info
   background: $theme-color
   padding: 0.75em 1em 0.1em
 
-.character-menu-status
-  background: darken($theme-color, 20%)
+.status
+  background: theme-darker(20%)
   font-size: 80%
   font-style: italic
   padding: 0.3rem 0.6rem
 
-.character-menu-friend
+.friend
   font-size: 80%
   font-style: italic
   padding: 0.3em 0.6em
@@ -88,4 +101,15 @@ export default {
 
   i
     opacity: 0.7
+
+nav
+  a
+    display: block
+    padding: 0.5em 0.6em
+    background: theme-darker(30%)
+    +animate()
+      background: theme-darker(45%)
+
+  i
+    width: 1.2em
 </style>
