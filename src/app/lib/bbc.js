60616353ce4f680879ee4bb26bb383d04f66974d
Kingdaro <kingdaro@gmail.com>
2016-10-06 17:16:27 -0400

    added BBC parser

--- src/app/lib/bbc.js
+++ src/app/lib/bbc.js
@@ -2,58 +2,33 @@
 import {getProfileURL, getAvatarURL, getExtendedIcon} from '../lib/f-list'
 import {parse as parseURL} from 'url'
 import {jwerty} from 'jwerty'
+import {createParser} from 'bbc.js'
 import path from 'path'
 
-export function parseBBC(input: string): string {
-  input = input.replace(/\b((?:http)(?:s)?(?::\/\/)[^\s\}\]\)]+)/gi, (match, url, offset) => {
-    const behind = input.substring(offset - 5, offset)
-    if (behind !== '[url]' && behind !== '[url=') {
-      return formatLink(url, url)
-    }
-    return match
-  })
+const parser = createParser({
+  b: { render: text => `<span class="bbc-bold">${text}</span>` },
+  i: { render: text => `<span class="bbc-italic">${text}</span>` },
+  u: { render: text => `<span class="bbc-underline">${text}</span>` },
+  s: { render: text => `<span class="bbc-strike">${text}</span>` },
+  sup: { render: text => `<span class="bbc-super">${text}</span>` },
+  sub: { render: text => `<span class="bbc-sub">${text}</span>` },
+  url: { render: (text, url) => formatLink(url, text) },
+  color: {
+    render: (text, color) => color ? `<span class="bbc-color-${color}">${text}</span>` : text,
+  },
+  noparse: {
+    render: text => text,
+    deep: false,
+  },
+  icon: { render: name => formatUserIcon(name) },
+  eicon: { render: name => formatExtendedIcon(name) },
+  user: { render: name => formatUserIcon(name) },
+  session: { render: (name, id) => formatPrivateChannel(id, name) },
+  channel: { render: name => formatPublicChannel(name) },
+})
 
-  const tags = {
-    i: /(?:\[i])([^]*?)(?:\[\/i])/gi,
-    b: /(?:\[b])([^]*?)(?:\[\/b])/gi,
-    u: /(?:\[u])([^]*?)(?:\[\/u])/gi,
-    s: /(?:\[s])([^]*?)(?:\[\/s])/gi,
-    sup: /(?:\[sup])([^]*?)(?:\[\/sup])/gi,
-    sub: /(?:\[sub])([^]*?)(?:\[\/sub])/gi,
-    color: /(?:\[color=)([a-z]+)(?:])([^]*?)(?:\[\/color])/gi,
-    url: /(?:\[url])([^]*?)(?:\[\/url])/gi,
-    urlhref: /(?:\[url=)(.+?)(?:])([^]*?)(?:\[\/url])/gi,
-    channel: /(?:\[channel])([^]+?)(?:\[\/channel])/gi,
-    session: /(?:\[session=)(.+?)(?:])([^]*)(?:\[\/session])/gi,
-    icon: /(?:\[icon])(.+?)(?:\[\/icon])/gi,
-    user: /(?:\[user])(.+?)(?:\[\/user])/gi,
-    eicon: /(?:\[eicon])(.+?)(?:\[\/eicon])/gi,
-  }
-
-  const replacers = {
-    i: (_, text) => `<span class='bbc-italic'>${parseBBC(text)}</span>`,
-    b: (_, text) => `<span class='bbc-bold'>${parseBBC(text)}</span>`,
-    u: (_, text) => `<span class='bbc-underline'>${parseBBC(text)}</span>`,
-    s: (_, text) => `<span class='bbc-strike'>${parseBBC(text)}</span>`,
-    sup: (_, text) => `<span class='bbc-super'>${parseBBC(text)}</span>`,
-    sub: (_, text) => `<span class='bbc-sub'>${parseBBC(text)}</span>`,
-    color: (_, color, text) => `<span class='bbc-color-${color}'>${parseBBC(text)}</span>`,
-    url: (_, url) => formatLink(url, url),
-    urlhref: (_, url, text) => formatLink(url, text),
-    channel: (_, channel) => formatPublicChannel(channel),
-    session: (_, name, id) => formatPrivateChannel(id, name),
-    icon: (_, user) => formatUserIcon(user),
-    user: (_, user) => formatUserIcon(user),
-    eicon: (_, icon) => formatExtendedIcon(icon),
-  }
-
-  for (const tag in tags) {
-    const exp = tags[tag]
-    const replacer = replacers[tag]
-    input = input.replace(exp, replacer)
-  }
-
-  return input
+export function parseBBC(input: string) {
+  return parser.parse(input)
 }
 
 export function doBBCShortcut(text: string, event: Object) {
@@ -70,11 +45,9 @@ export function doBBCShortcut(text: string, event: Object) {
     event.preventDefault()
 
     const {selectionStart: start, selectionEnd: end} = event.target
-
     const before = text.substring(0, start)
     const between = text.substring(start, end)
     const after = text.substring(end)
-
     return `${before}[${tag}]${between}[/${tag}]${after}`
   }
   return text
