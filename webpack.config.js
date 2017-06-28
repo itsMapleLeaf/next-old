501362110bf6ac8592e596c105773271a54d3b12
just nobody <kingdaro@gmail.com>
2017-06-28 19:53:12 -0400

    modernized like a mofo

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const webpack = require('webpack')
-const {join} = require('path')
+const { join } = require('path')
 
 const config = {
-  entry: './app/main.js',
+  entry: './src/main',
   output: {
     path: join(__dirname, 'dist'),
     publicPath: 'dist/',
@@ -10,32 +10,22 @@ const config = {
   },
   module: {
     loaders: [
-      { test: /\.vue$/, loader: 'vue' },
-      { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
-      { test: /\.json$/, loader: 'json' },
-      { test: /\.mp3$|.ogg$/, loader: 'file' },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            stylus: 'style-loader!css-loader!stylus-loader?paths=./src/styles',
+          },
+        },
+      },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.mp3$|.ogg$/, loader: 'file-loader' },
     ],
   },
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      options: {
-        vue: {
-          loaders: {
-            stylus: 'style!css!stylus?paths=./app/styles'
-          }
-        }
-      }
-    })
-  ],
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  config.plugins = (config.plugins || []).concat([
-    new webpack.NamedModulesPlugin({
-      context: 'app',
-    }),
-  ])
-  config.devtool = '#inline-source-map'
+  plugins: [new webpack.NamedModulesPlugin()],
+  devtool: 'source-map',
 }
 
 module.exports = config
