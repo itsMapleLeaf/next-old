f17f29b2d382f3814ff00a62b743e2605e82887f
just nobody <kingdaro@gmail.com>
2017-06-30 17:43:49 -0400

    added ability to react to overlay close action

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react'
+import { MouseEvent } from '../lib/react-utils'
 import './Overlay.css'
 
 export default class Overlay extends React.Component {
+  props: {
+    onClose?: () => any
+    children?: React.ReactNode
+  }
+
+  shade: HTMLElement | null
+
+  handleClick = (event: MouseEvent) => {
+    if (event.currentTarget === this.shade && this.props.onClose != null) {
+      this.props.onClose()
+    }
+  }
+
   render() {
     return (
-      <div className="overlay-shade fullscreen flex-column">
+      <div
+        className="overlay-shade fullscreen flex-column"
+        ref={el => (this.shade = el)}
+        onClick={this.handleClick}
+      >
         <div className="overlay-content-container bg-1">
           {this.props.children}
         </div>
