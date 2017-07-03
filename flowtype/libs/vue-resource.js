de1cd9ebec6a2f00baa430f917e8b51721e85094
just nobody <kingdaro@gmail.com>
2017-07-02 23:00:25 -0400

    start over for now

--- flowtype/libs/vue-resource.js
+++
@@ -1,8 +0,0 @@
-declare module 'vue-resource' {
-  declare class VueResource {
-    static options: Object,
-    static post: Function
-  }
-
-  declare var exports: typeof VueResource
-}
--- src/store/index.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export * as store from './store'
-export {state} from './state'
-export {getters} from './getters'
