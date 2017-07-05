db51cf1a4db175c3e4640e7d81594846484e05ed
just nobody <kingdaro@gmail.com>
2017-07-05 18:47:08 -0400

    add more store functions

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,4 @@
+import * as forage from 'localforage'
 import { action, observable } from 'mobx'
 import { fetchCharacters, fetchTicket } from './lib/f-list'
 
@@ -5,6 +6,19 @@ export default class Store {
   account = ''
   ticket = ''
   @observable userCharacters = [] as string[]
+  identity = ''
+
+  @action
+  async loadUserData() {
+    this.account = (await forage.getItem<string>('account')) || ''
+    this.ticket = (await forage.getItem<string>('ticket')) || ''
+  }
+
+  @action
+  async saveUserData() {
+    await forage.setItem('account', this.account)
+    await forage.setItem('ticket', this.ticket)
+  }
 
   @action
   async login(account: string, password: string) {
@@ -16,4 +30,12 @@ export default class Store {
   async fetchUserCharacters() {
     this.userCharacters = await fetchCharacters(this.account, this.ticket)
   }
+
+  @action
+  setIdentity(identity: string) {
+    this.identity = identity
+  }
+
+  @action
+  connect() {}
 }
