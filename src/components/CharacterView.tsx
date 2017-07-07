91cd01e139c2579f1ee96fef95b55d0da31c5547
just nobody <kingdaro@gmail.com>
2017-07-07 18:17:48 -0400

    added colored status dots

--- src/components/CharacterView.tsx
+++ src/components/CharacterView.tsx
@@ -3,12 +3,16 @@ import { Character } from '../store/chat-state'
 import './CharacterView.css'
 
 export default function CharacterView(props: { character: Character }) {
-  const { name, gender } = props.character
+  const { name, gender, status } = props.character
   return (
     <a href="#" style={{ fontWeight: 500 }}>
-      <span className={`character-gender-${gender.toLowerCase()}`}>
-        {name}
-      </span>
+      <span
+        className={`character-status-${status.toLowerCase()}`}
+        style={{ transform: 'scale(1.3)', display: 'inline-block' }}
+      >
+        &bull;
+      </span>{' '}
+      <span className={`character-gender-${gender.toLowerCase()}`}>{name}</span>
     </a>
   )
 }
