0e4536558d04edc99d093faabadf5a72f6859de1
just nobody <kingdaro@gmail.com>
2017-07-06 23:10:19 -0400

    refactored the shit out of this

--- src/components/ChannelView.tsx
+++ src/components/ChannelView.tsx
@@ -1,21 +1,36 @@
+import sortBy from 'lodash/sortBy'
 import { observer } from 'mobx-react'
 import * as React from 'react'
-import { Channel } from '../store/chat-state'
+import { Channel, Character, Message } from '../store/chat-state'
 import MessageView from './MessageView'
+
+function sortUserList(users: Character[]) {
+  return sortBy(users, 'name')
+}
+
+function renderMessageList(messages: Message[]) {
+  return messages.map(msg => <MessageView message={msg} key={msg.id} />)
+}
+
+function renderUserList(users: Character[]) {
+  return users.map(user =>
+    <div key={user.name} style={{ width: '12em', margin: '0.4em 0.5em' }}>
+      {user.name}
+    </div>
+  )
+}
 
 function ChannelView(props: { channel: Channel }) {
   const { messages, users } = props.channel
+  const userList = Array.from(users.values())
+  const sortedUsers = sortUserList(userList)
   return (
     <div className="flex-row" style={{ width: '100%', height: '100%' }}>
       <div className="flex-grow scroll-v bg-2">
-        {messages.map(msg => <MessageView message={msg} key={msg.id} />)}
+        {renderMessageList(messages)}
       </div>
       <div className="scroll-v">
-        {Array.from(users.values()).map(user =>
-          <div key={user.name} style={{ width: '12em', margin: '0.4em 0.5em' }}>
-            {user.name}
-          </div>
-        )}
+        {renderUserList(sortedUsers)}
       </div>
     </div>
   )
