6ab901b81d5418ae73405d2849a5ed6a5bbc7bc4
just nobody <kingdaro@gmail.com>
2017-07-06 21:56:45 -0400

    show chat tabs

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,6 +1,25 @@
+import { observable } from 'mobx'
 import { observer } from 'mobx-react'
 import * as React from 'react'
+import { preventDefault } from '../lib/react-utils'
 import Store from '../store'
+import './Chat.css'
+
+const chatTabStyle: React.CSSProperties = {
+  width: '12em',
+  padding: '0.4em 0.6em',
+  display: 'block',
+}
+
+function ChatTab(props: { title: any; active: boolean; onClick: () => any }) {
+  const activeClass = props.active ? 'bg-1' : ''
+  const onMouseDown = preventDefault(props.onClick)
+  return (
+    <a href="#" style={chatTabStyle} className={activeClass} onMouseDown={onMouseDown}>
+      {props.title}
+    </a>
+  )
+}
 
 @observer
 export default class Chat extends React.Component {
@@ -9,17 +28,24 @@ export default class Chat extends React.Component {
   }
 
   store = this.props.store
+  @observable tabIndex = 0
 
   render() {
     const channels = Array.from(this.store.chat.channels.values())
+
+    const tabs = channels.map((ch, index) =>
+      <ChatTab
+        key={ch.id}
+        title={ch.title}
+        active={index === this.tabIndex}
+        onClick={() => (this.tabIndex = index)}
+      />
+    )
+
     return (
       <div className="fullscreen flex-row">
-        <div>
-          {channels.map(ch =>
-            <div key={ch.id}>
-              {ch.title}
-            </div>
-          )}
+        <div className="bg-2">
+          {tabs}
         </div>
         <div className="flex-grow">i am chat</div>
       </div>
