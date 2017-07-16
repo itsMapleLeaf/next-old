b0b6e665541adc4f4d6523ebd42d2efed95caf6e
just nobody <kingdaro@gmail.com>
2017-07-16 04:12:38 -0400

    rip out the build system and roll my own since i know what I'm doing, and react-scripts-ts has too many dependencies and is impossible to debug when something mucks up

---
+++ webpack.config.ts
@@ -0,0 +1,32 @@
+import * as webpack from 'webpack'
+import * as HtmlPlugin from 'html-webpack-plugin'
+import { resolve } from 'path'
+
+const config: webpack.Configuration = {
+  entry: {
+    app: './src/index',
+    react: ['react', 'react-dom'],
+  },
+  output: {
+    path: resolve(__dirname, 'build'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.svg$/, loader: 'file-loader' },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  plugins: [
+    new HtmlPlugin({ template: './src/index.html' }),
+    new webpack.optimize.CommonsChunkPlugin({ names: ['react'] }),
+    new webpack.NamedModulesPlugin(),
+  ],
+  devtool: 'source-map',
+}
+
+export default config
