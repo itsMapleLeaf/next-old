6514d9beb8a5005ae7d3fc1a5f25c2fe1bfca481
just nobody <kingdaro@gmail.com>
2017-07-17 09:05:07 -0400

    actual proper error handling, mostly

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <login v-if="showLogin" @submit="handleLoginSubmit"></login>
-    {{ account }} {{ ticket }} {{ characters }}
+    <overlay v-if="showLogin">
+      <login @submit="handleLoginSubmit" :status="loginStatus"></login>
+    </overlay>
   </div>
 </template>
 
@@ -9,15 +10,17 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import Login from './Login.vue'
+import Overlay from './Overlay.vue'
 import * as api from '../lib/api'
 
 @Component({
-  components: { Login }
+  components: { Login, Overlay }
 })
 export default class extends Vue {
   account = ''
   ticket = ''
   characters = [] as string[]
+  loginStatus = ''
 
   showLogin = false
 
@@ -27,12 +30,14 @@ export default class extends Vue {
 
   async handleLoginSubmit(account: string, password: string) {
     this.account = account
+    this.loginStatus = ''
     try {
       this.ticket = await api.fetchTicket(account, password)
       this.characters = await api.fetchCharacterList(account, this.ticket)
       this.showLogin = false
     } catch (err) {
       this.showLogin = true
+      this.loginStatus = err.toString()
     }
   }
 }
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,22 +1,30 @@
 <template>
-  <form @submit.prevent="submit">
-    <fieldset>
-      <input type="text" placeholder="Username" v-model="username">
-    </fieldset>
-    <fieldset>
-      <input type="password" placeholder="••••••••" v-model="password">
-    </fieldset>
-    <fieldset>
-      <button action="submit">Log in</button>
-    </fieldset>
-  </form>
+  <div>
+    <h1>Hello, beautiful.</h1>
+    <form @submit.prevent="submit">
+      <fieldset>
+        <input type="text" placeholder="Username" v-model="username">
+      </fieldset>
+      <fieldset>
+        <input type="password" placeholder="••••••••" v-model="password">
+      </fieldset>
+      <fieldset>
+        <button action="submit">Log in</button>
+      </fieldset>
+    </form>
+    <p>{{ status }}</p>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
-@Component
+@Component({
+  props: {
+    status: String
+  }
+})
 export default class extends Vue {
   username = ''
   password = ''
