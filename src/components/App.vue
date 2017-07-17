4c170d10c45374955b269ad091d04f0247acc86a
just nobody <kingdaro@gmail.com>
2017-07-17 06:03:05 -0400

    added API functionality

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,19 +1,28 @@
 <template>
-  <login @submit="handleLoginSubmit">
-  </login>
+  <div>
+    <login @submit="handleLoginSubmit"></login>
+    {{ account }} {{ ticket }} {{ characters }}
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
 import Login from './Login.vue'
+import * as api from '../lib/api'
 
 @Component({
   components: { Login }
 })
 export default class extends Vue {
-  handleLoginSubmit(username: string, password: string) {
-    console.log(username, password)
+  account = ''
+  ticket = ''
+  characters = [] as string[]
+
+  async handleLoginSubmit(account: string, password: string) {
+    this.account = account
+    this.ticket = await api.fetchTicket(account, password)
+    this.characters = await api.fetchCharacterList(account, this.ticket)
   }
 }
 </script>
---
+++ src/lib/api.ts
@@ -0,0 +1,20 @@
+const ticketEndpoint = 'https://www.f-list.net/json/getApiTicket.php'
+
+const characterListEndpoint =
+  'https://www.f-list.net/json/api/character-list.php'
+
+export async function fetchTicket(account: string, password: string) {
+  const query = `?account=${account}&password=${password}`
+  const res = await fetch(ticketEndpoint + query)
+  const data = await res.json()
+  if (data.error) throw data.error
+  return data.ticket as string
+}
+
+export async function fetchCharacterList(account: string, ticket: string) {
+  const query = `?account=${account}&ticket=${ticket}`
+  const res = await fetch(characterListEndpoint + query)
+  const data = await res.json()
+  if (data.error) throw data.error
+  return data.characters as string[]
+}
