fc36a2ac7d29d15d2ac6eda49f8afa3ebd9facc1
just nobody <kingdaro@gmail.com>
2017-07-17 08:57:17 -0400

    split the fade effect into a component

---
+++ src/components/Fade.vue
@@ -0,0 +1,24 @@
+<template>
+  <transition name="fade" appear>
+    <slot></slot>
+  </transition>
+</template>
+
+<style scoped>
+@keyframes fade-in {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+.fade-enter-active {
+  animation: fade-in 0.3s;
+}
+
+.fade-leave-active {
+  animation: fade-in 0.3s reverse;
+}
+</style>
