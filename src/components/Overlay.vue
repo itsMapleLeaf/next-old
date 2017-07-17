213986c45ccce9389a026f89087138aabd8def9e
just nobody <kingdaro@gmail.com>
2017-07-17 08:41:22 -0400

    added simple overlay component

---
+++ src/components/Overlay.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="overlay-shade">
+    <div class="overlay-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style>
+.overlay-shade {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+
+  background-color: rgba(0, 0, 0, 0.5);
+
+  display: flex;
+  flex-direction: column;
+}
+
+.overlay-content {
+  margin: auto;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
+}
+</style>
