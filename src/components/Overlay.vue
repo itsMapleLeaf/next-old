189acf521177c2b20dba522446fb35c777fc5c12
just nobody <kingdaro@gmail.com>
2017-07-17 09:05:48 -0400

    take out transition stuff, export empty object so TS doesn't complain

--- src/components/Overlay.vue
+++ src/components/Overlay.vue
@@ -1,14 +1,16 @@
 <template>
-  <transition name="fade" appear>
-    <div class="overlay-shade">
-      <div class="overlay-content">
-        <slot></slot>
-      </div>
+  <div class="overlay-shade">
+    <div class="overlay-content">
+      <slot></slot>
     </div>
-  </transition>
+  </div>
 </template>
 
-<style>
+<script lang="ts">
+export default {}
+</script>
+
+<style scoped>
 .overlay-shade {
   position: fixed;
   left: 0;
@@ -25,22 +27,5 @@
 .overlay-content {
   margin: auto;
   box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.5);
-}
-
-@keyframes fade-in {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-
-.fade-enter-active {
-  animation: fade-in 0.3s;
-}
-
-.fade-leave-active {
-  animation: fade-in 0.3s reverse;
 }
 </style>
