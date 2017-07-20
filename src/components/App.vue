f8f0d579fbc3bc03b486b0e9b929576c26b718e5
just nobody <kingdaro@gmail.com>
2017-07-20 06:56:24 -0400

    enabled attrivbute wrapping in vetur formatting

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,8 +2,12 @@
   <div>
     <fade v-for="(overlay, i) in overlays" :key="i">
       <overlay>
-        <login v-if="overlay === 'login'" @submit="handleLoginSubmit" :status="loginStatus" class="text-center"></login>
-        <character-select v-if="overlay === 'characterSelect'" :characters="store.user.characters" @submit="handleIdentitySubmit" class="text-center" style="padding: 0 1em 1em"></character-select>
+        <login v-if="overlay === 'login'" @submit="handleLoginSubmit" :status="loginStatus"
+          class="text-center">
+        </login>
+        <character-select v-if="overlay === 'characterSelect'" :characters="store.user.characters"
+          @submit="handleIdentitySubmit" class="text-center" style="padding: 0 1em 1em">
+        </character-select>
       </overlay>
     </fade>
   </div>
