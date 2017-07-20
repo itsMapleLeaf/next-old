fa71a9e2c41a8a8c600df0761ce9eb564ef5c431
just nobody <kingdaro@gmail.com>
2017-07-20 06:27:06 -0400

    fixed app template giving incorrect reference to character list

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,7 +2,7 @@
   <div>
     <overlay v-for="(overlay, i) in overlays" :key="i">
       <login v-if="overlay === 'login'" @submit="handleLoginSubmit" :status="loginStatus"></login>
-      <character-select v-if="overlay === 'characterSelect'" :characters="store.userCharacters" @submit="handleIdentitySubmit"></character-select>
+        <character-select v-if="overlay === 'characterSelect'" :characters="store.user.characters" @submit="handleIdentitySubmit"></character-select>
     </overlay>
   </div>
 </template>
