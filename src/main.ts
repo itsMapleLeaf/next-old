999b3b1b3d59b9d9b02a61843f1a9032253b6580
just nobody <kingdaro@gmail.com>
2017-07-29 13:32:17 -0400

    clean up some comments

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,12 @@
 import 'regenerator-runtime/runtime'
 import Vue from 'vue'
-// import VueResource from 'vue-resource'
 import App from './components/App.vue'
 import { store, state } from './store'
 
-// configure keycodes
-// Vue.config.keyCodes = {
-//   enter: 13,
-//   space: 32,
-// }
-
-// configure vue resource
-// Vue.use(VueResource)
-// Vue.http.options.emulateJSON = true
-
-// initialize the application store (jumpstart the app)
-store.initialize()
-
-// create vue instance
-/* eslint no-new: off */
 new Vue({
   el: '#app',
   render: h => h(App),
   data: { state },
 })
+
+store.initialize()
