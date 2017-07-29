43e5e557ab9e7b29d64d3a6c779d6bb30ca97262
just nobody <kingdaro@gmail.com>
2017-07-29 17:57:15 -0400

    try out using glamor/glamorous for css in JS

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,5 +1,26 @@
 import * as React from 'react'
-import css from './Toggle.styl?module'
+import * as glamor from 'glamor'
+
+const styles = {
+  toggle: glamor.css({
+    fontSize: '90%',
+    opacity: 0.7,
+    borderBottom: '1px solid transparent',
+    transition: '0.2s',
+
+    ':hover': {
+      opacity: 1,
+      borderBottomColor: 'hsla(0, 0%, 100%, 0.3)',
+    },
+  }),
+  checked: glamor.css({
+    opacity: 0.8,
+  }),
+  disabled: glamor.css({
+    opacity: 0.3,
+    pointerEvents: 'none',
+  }),
+}
 
 export default class Toggle extends React.Component {
   props: {
@@ -9,15 +30,17 @@ export default class Toggle extends React.Component {
   }
 
   render() {
-    const checkedClass = this.props.value ? css.checked : ''
-    const disabledClass = this.props.disabled ? css.disabled : ''
+    const checkedClass = this.props.value ? styles.checked : ''
+    const disabledClass = this.props.disabled ? styles.disabled : ''
 
     const iconClass = this.props.value
       ? 'mdi-checkbox-marked-outline'
       : 'mdi-checkbox-blank-outline'
 
+    const classes = `${styles.toggle} ${checkedClass} ${disabledClass}`
+
     return (
-      <a className={`${css.toggle} ${checkedClass} ${disabledClass}`} href="#">
+      <a className={classes} href="#">
         <i className={`mdi ${iconClass}`} /> {this.props.children}
       </a>
     )
