0e194559570559aabd66b03eb4f8d1b3acd33131
just nobody <kingdaro@gmail.com>
2017-07-29 14:32:14 -0400

    added the disabled functionality

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -4,16 +4,20 @@ import css from './Toggle.styl?module'
 export default class Toggle extends React.Component {
   props: {
     value: boolean
+    disabled: boolean
     children: React.ReactNode
   }
 
   render() {
+    const checkedClass = this.props.value ? css.checked : ''
+    const disabledClass = this.props.disabled ? css.disabled : ''
+
     const iconClass = this.props.value
       ? 'mdi-checkbox-marked-outline'
       : 'mdi-checkbox-blank-outline'
 
     return (
-      <a className={css.toggle} href="#">
+      <a className={`${css.toggle} ${checkedClass} ${disabledClass}`} href="#">
         <i className={`mdi ${iconClass}`} /> {this.props.children}
       </a>
     )
