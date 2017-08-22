dec1d8ff050c8919d25caf294fad14f1126d3486
just nobody <kingdaro@gmail.com>
2017-08-22 02:03:53 -0400

    use a directive to make the menu "fit on screen" depending on where it's opened

--- src/components/chat/ContextMenu.vue
+++ src/components/chat/ContextMenu.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="context-menu bg-color-darken-1" :style="{ left: x + 'px', top: y + 'px' }">
+  <div class="context-menu bg-color-darken-1" v-fit-on-screen="{ x, y }">
     <slot></slot>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
   props: {
     x: Number,
     y: Number
+  },
+  directives: {
+    fitOnScreen(el, binding) {
+      Vue.nextTick(() => {
+        const rect = el.getBoundingClientRect()
+        const pos = binding.value
+        const left = pos.x + rect.width < window.innerWidth ? pos.x : pos.x - rect.width
+        const top = pos.y + rect.height < window.innerHeight ? pos.y : pos.y - rect.height
+        el.style.left = left + 'px'
+        el.style.top = top + 'px'
+      })
+    }
   }
 }
 </script>
@@ -17,5 +31,6 @@ export default {
 .context-menu {
   position: fixed;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.5);
+  white-space: nowrap;
 }
 </style>
