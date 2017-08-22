08450a7120e825206a926293f37416d8bddee32f
just nobody <kingdaro@gmail.com>
2017-08-22 14:33:21 -0400

    give chat tab content a facelift - restyle/restructure more consistently - added an avatar icon for private chat tabs

--- src/components/chat/ChatTabContent.vue
+++ src/components/chat/ChatTabContent.vue
@@ -1,22 +1,49 @@
 <template>
-  <span>
+  <span class="flex-row flex-align-center">
     <template v-if="type === 'channel'">
-      <icon>{{ channel.id === channel.title ? 'earth' : 'key-variant' }}</icon>
-      <span v-html="channel.title"></span>
+      <span class="icon">
+        <icon>{{ channel.id === channel.title ? 'earth' : 'key-variant' }}</icon>
+      </span>
+      <span class="title" v-html="channel.title"></span>
     </template>
     <template v-if="type === 'privateChat'">
-      <icon>account</icon>
-      <span v-html="privateChat.partner"></span>
+      <span class="icon">
+        <img :src="getAvatarURL(privateChat.partner)" class="private-chat-avatar">
+      </span>
+      <span class="title" v-html="privateChat.partner"></span>
     </template>
   </span>
 </template>
 
 <script>
+import { getAvatarURL } from '../../api'
+
 export default {
   props: {
     type: String,
     channel: [Object, null],
     privateChat: [Object, null],
+  },
+  methods: {
+    getAvatarURL
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.private-chat-avatar {
+  width: 1em;
+  height: 1em;
+}
+
+.title {
+  margin-left: 0.3rem;
+}
+
+.icon {
+  img,
+  i {
+    display: block;
+  }
+}
+</style>
