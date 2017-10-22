3a9a6f4212ca6b40a96c1179ab51ebb17b7b0c1b
kingdaro <kingdaro@gmail.com>
2017-10-22 17:23:45 -0400

    this reads a little better

--- src/app/components/Icon.tsx
+++ src/app/components/Icon.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 
-type IconProps = JSX.IntrinsicElements['img'] & {
+type IconProps = {
   size?: number
   name: string
 }
 
-export function Icon(props: IconProps) {
+export function Icon(props: IconProps & JSX.IntrinsicElements['img']) {
   const { size = 18, name, ...imgProps } = props
   const style = { width: size + 'px', height: size + 'px' }
   return <img style={style} src={`/public/icons/${name}.svg`} alt="" {...imgProps} />
