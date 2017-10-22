5eb6dc1a6022dbd2ef79f22487926a7ce321e084
kingdaro <kingdaro@gmail.com>
2017-10-22 17:39:21 -0400

    use a proper svg object element for icons

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,7 +1,7 @@
 import { inject } from 'mobx-react'
 import * as React from 'react'
+import { Loading } from 'src/app/components/Loading'
 import { AppStore } from 'src/app/stores/AppStore'
-import { Overlay } from 'src/common/components/Overlay'
 
 type AppProps = {
   store?: AppStore
@@ -12,12 +12,7 @@ export class App extends React.Component<AppProps> {
   render() {
     return (
       <main className="fullscreen flex-center bg-color-main text-color-main">
-        {/* <Loading>hodor</Loading> */}
-        <Overlay>
-          <div className="bg-color-main" style={{ padding: '1rem' }}>
-            ye
-          </div>
-        </Overlay>
+        <Loading text="hodor" />
       </main>
     )
   }
--- src/app/components/Icon.tsx
+++ src/app/components/Icon.tsx
@@ -5,8 +5,15 @@ type IconProps = {
   name: string
 }
 
-export function Icon(props: IconProps & JSX.IntrinsicElements['img']) {
-  const { size = 18, name, ...imgProps } = props
-  const style = { width: size + 'px', height: size + 'px' }
-  return <img style={style} src={`/public/icons/${name}.svg`} alt="" {...imgProps} />
+export function Icon(props: IconProps & JSX.IntrinsicElements['object']) {
+  const { size = 18, name, style, ...otherProps } = props
+  const iconPath = `public/icons/${name}.svg`
+
+  const iconStyle = {
+    width: size + 'px',
+    height: size + 'px',
+    ...style,
+  }
+
+  return <object type="image/svg+xml" data={iconPath} style={iconStyle} {...otherProps} />
 }
