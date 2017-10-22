9463971ec3aae929ffcc043bc13f646acba45ec2
kingdaro <kingdaro@gmail.com>
2017-10-21 21:14:54 -0400

    optimize production build

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,6 @@
 import 'normalize.css/normalize.css'
 
-import './styles/colors.scss'
-import './styles/fade-transition.scss'
-import './styles/flex.scss'
-import './styles/global.scss'
-import './styles/helpers.scss'
-import './styles/bbc.scss'
-import './styles/character-colors.scss'
+import './styles/index.scss'
 
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,52 +1,114 @@
 import * as webpack from 'webpack'
-import * as HTMLWebpackPlugin from 'html-webpack-plugin'
+import * as HTMLPlugin from 'html-webpack-plugin'
+import * as CopyPlugin from 'copy-webpack-plugin'
+import * as ExtractTextPlugin from 'extract-text-webpack-plugin'
+import * as merge from 'webpack-merge'
 import { resolve } from 'path'
+
+const CleanPlugin = require('clean-webpack-plugin')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+const ScriptExtHtmlPlugin = require('script-ext-html-webpack-plugin')
 
 const root = resolve(__dirname)
 const sourcePath = resolve(root, 'src')
 const outputPath = resolve(root, 'build')
 
-const tsLoader = {
-  test: /\.tsx?$/,
-  loader: 'ts-loader',
-  options: {
-    compilerOptions: { module: 'esnext' },
-  },
+type ConfigEnvironment = {
+  production?: boolean
 }
 
-const cssLoader = {
-  test: /\.css$/,
-  loader: 'style-loader!css-loader',
-}
-
-const sassLoader = {
-  test: /\.s(c|a)ss$/,
-  loader: 'style-loader!css-loader!sass-loader',
-}
-
-const config: webpack.Configuration = {
-  entry: {
-    app: resolve(sourcePath, 'main'),
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: outputPath,
-  },
-  module: {
-    rules: [tsLoader, cssLoader, sassLoader],
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './index.html',
-    }),
-  ],
-  resolve: {
-    extensions: ['.js', '.json', '.ts', '.tsx'],
-    alias: {
-      '@': sourcePath,
+export = (env = {} as ConfigEnvironment) => {
+  const tsLoader: webpack.Rule = {
+    test: /\.tsx?$/,
+    loader: 'ts-loader',
+    options: {
+      compilerOptions: { module: 'esnext' },
     },
-  },
-  // devtool: 'eval-source-map',
-}
+    include: [sourcePath],
+  }
 
-export = config
+  const cssLoader = {
+    loader: 'css-loader',
+    options: {
+      minimize: env.production,
+    },
+  }
+
+  const cssLoaderRule: webpack.Rule = {
+    test: /\.css$/,
+    use: ExtractTextPlugin.extract({
+      use: [cssLoader],
+      fallback: 'style-loader',
+    }),
+  }
+
+  const sassLoaderRule: webpack.Rule = {
+    test: /\.s(c|a)ss$/,
+    use: ExtractTextPlugin.extract({
+      use: [cssLoader, 'sass-loader'],
+      fallback: 'style-loader',
+    }),
+  }
+
+  const baseConfig: webpack.Configuration = {
+    entry: {
+      app: resolve(sourcePath, 'main'),
+      lib: ['react', 'react-dom', 'mobx', 'mobx-react', 'localforage', 'bbc.js'],
+    },
+    output: {
+      filename: '[name].js',
+      path: outputPath,
+    },
+    module: {
+      rules: [tsLoader, cssLoaderRule, sassLoaderRule],
+    },
+    plugins: [
+      new HTMLPlugin({
+        template: './index.html',
+        chunksSortMode: 'dependency',
+      }),
+      new ScriptExtHtmlPlugin({
+        defaultAttribute: 'defer',
+      }),
+      new CleanPlugin(['build'], { verbose: false }),
+      new ExtractTextPlugin({
+        filename: '[name].css',
+        disable: !env.production,
+      }),
+    ],
+    resolve: {
+      extensions: ['.js', '.json', '.ts', '.tsx'],
+      alias: {
+        '@': sourcePath,
+      },
+    },
+  }
+
+  const devConfig: webpack.Configuration = {
+    plugins: [new webpack.NamedModulesPlugin()],
+  }
+
+  const prodConfig: webpack.Configuration = {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"',
+      }),
+      new CopyPlugin([
+        {
+          from: 'public',
+          to: 'public',
+        },
+      ]),
+      new UglifyPlugin(),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['lib'],
+      }),
+    ],
+  }
+
+  if (env.production) {
+    return merge(baseConfig, prodConfig)
+  }
+  return merge(baseConfig, devConfig)
+}
