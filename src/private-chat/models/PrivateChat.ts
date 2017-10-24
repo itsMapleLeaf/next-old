d3c6442cf8c3afb1e98f1abb93f21b77e206bf18
kingdaro <kingdaro@gmail.com>
2017-10-24 19:48:03 -0400

    make PC messages reactive before we forget

--- src/private-chat/models/PrivateChat.ts
+++ src/private-chat/models/PrivateChat.ts
@@ -1,7 +1,8 @@
+import { observable } from 'mobx'
 import { Message } from 'src/message/models/Message'
 
 export class PrivateChat {
-  messages = [] as Message[]
+  @observable messages = [] as Message[]
 
   constructor(public partner: string) {}
 }
