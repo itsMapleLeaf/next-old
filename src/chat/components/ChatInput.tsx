f19fb54d0773bcc23938b4082329a01a2f756a85
kingdaro <kingdaro@gmail.com>
2017-10-24 14:20:47 -0400

    added send button to ChatInput

--- src/chat/components/ChatInput.tsx
+++ src/chat/components/ChatInput.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react'
 
-export function ChatInput(props: JSX.IntrinsicElements['textarea']) {
+export function ChatInput(props: JSX.IntrinsicElements['div']) {
+  const { className, ...divProps } = props
   return (
-    <textarea
-      className="padding"
-      placeholder="Say something..."
-      style={{ resize: 'none' }}
-      {...props}
-    />
+    <div className={`flex-row padding ${className}`} {...divProps}>
+      <textarea
+        className="flex-grow padding margin-right"
+        placeholder="Say something..."
+        style={{ resize: 'none' }}
+      />
+      <button style={{ width: '80px' }}>Send</button>
+    </div>
   )
 }
