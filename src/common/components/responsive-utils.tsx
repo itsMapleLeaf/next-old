ed679b5e8e8d75f6d78cb541062541d9ed866967
kingdaro <kingdaro@gmail.com>
2017-10-23 23:33:01 -0400

    added some utilities for responsive design

---
+++ src/common/components/responsive-utils.tsx
@@ -0,0 +1,4 @@
+import styled from 'styled-components'
+
+export const ShowOnDesktop = styled.div`@media (max-width: 750px) {display: none;}`
+export const ShowOnMobile = styled.div`@media (min-width: 750px) {display: none;}`
