8f5b8a1dcba77630649e2d1ba4a34e8079fa0339
kingdaro <kingdaro@gmail.com>
2017-10-24 01:17:22 -0400

    added close button to the chat tab

--- src/chat/components/ChannelTab.tsx
+++ src/chat/components/ChannelTab.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
+import { Icon } from 'src/app/components/Icon'
+
 type ChannelTabProps = {
   children: React.ReactNode
   active?: boolean
 }
 
-const Wrapper = styled.a`
-  padding: 8px;
-
+const Wrapper = styled.div`
   &:hover {
     opacity: ${(props: ChannelTabProps) => (props.active ? 1 : 0.75)};
   }
@@ -16,11 +16,29 @@ const Wrapper = styled.a`
   opacity: ${(props: ChannelTabProps) => (props.active ? 1 : 0.5)};
 `
 
+const ContentWrapper = styled.a`padding: 8px;`
+
+const CloseIcon = styled.a`
+  padding: 8px;
+  line-height: 0;
+
+  opacity: 0.3;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`
+
 export function ChatTab(props: ChannelTabProps) {
   const activeClass = props.active ? 'bg-color-main' : ''
   return (
-    <Wrapper href="#" style={{ padding: '8px' }} className={`${activeClass}`} active={props.active}>
-      {props.children}
+    <Wrapper className={`${activeClass} flex-row flex-align-stretch`} active={props.active}>
+      <ContentWrapper href="#" className="flex-grow">
+        {props.children}
+      </ContentWrapper>
+      <CloseIcon href="#" className="flex-center">
+        <Icon name="close" />
+      </CloseIcon>
     </Wrapper>
   )
 }
