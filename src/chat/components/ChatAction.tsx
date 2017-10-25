f8b43992e99fb93766187d499ffe0a17f32c129e
kingdaro <kingdaro@gmail.com>
2017-10-25 16:09:53 -0400

    allow ChatAction to accept onclick prop

--- src/chat/components/ChatAction.tsx
+++ src/chat/components/ChatAction.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import styled from 'styled-components'
 
 import { Icon } from 'src/app/components/Icon'
+import { preventDefault } from 'src/common/util/react'
 
 const Wrapper = styled.a`
   opacity: 0.5;
@@ -11,9 +12,13 @@ const Wrapper = styled.a`
   }
 `
 
-export function ChatAction(props: { icon: string }) {
+export function ChatAction(props: { icon: string; onClick?: () => void }) {
   return (
-    <Wrapper href="#" className="padding block no-line-height">
+    <Wrapper
+      href="#"
+      className="padding block no-line-height"
+      onClick={preventDefault(props.onClick)}
+    >
       <Icon name={props.icon} size={32} />
     </Wrapper>
   )
