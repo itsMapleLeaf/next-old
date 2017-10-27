1682742dfb7b63d947df5d0836a2ec0681536cfa
kingdaro <kingdaro@gmail.com>
2017-10-27 03:23:04 -0400

    MessageComponent: parse /me, render text as HTML

--- src/message/components/MessageComponent.tsx
+++ src/message/components/MessageComponent.tsx
@@ -3,6 +3,8 @@ import { CharacterName } from 'src/character/components/CharacterName'
 import { Message } from 'src/message/models/Message'
 import styled from 'styled-components'
 
+const actionExp = /^\s*\/me\s*/
+
 const Wrapper = styled.div`
   &:nth-child(2n) {
     background-color: rgba(0, 0, 0, 0.15);
@@ -10,12 +12,18 @@ const Wrapper = styled.div`
 `
 
 export function MessageComponent(props: { message: Message }) {
+  const { sender, text } = props.message
+  const isAction = text.trim().startsWith('/me')
+  const parsedText = text.replace(actionExp, '')
+
+  const actionClass = isAction ? 'text-italic' : ''
+
   return (
-    <Wrapper className="padding">
+    <Wrapper className={`padding ${actionClass}`}>
       <span className="margin-right">
         <CharacterName name={props.message.sender} />
       </span>
-      <span className="preserve-ws">{props.message.text}</span>
+      <span className={`preserve-ws`} dangerouslySetInnerHTML={{ __html: parsedText }} />
     </Wrapper>
   )
 }
