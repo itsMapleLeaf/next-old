472ed60bfbbf967c8d952025773b512b3e3c4830
kingdaro <kingdaro@gmail.com>
2017-10-28 18:06:07 -0400

    Message: added message type highlights

--- src/message/components/MessageComponent.tsx
+++ src/message/components/MessageComponent.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { CharacterName } from 'src/character/components/CharacterName'
-import { Message } from 'src/message/models/Message'
+import { Message, MessageType } from 'src/message/models/Message'
 import styled from 'styled-components'
 
 const actionExp = /^\s*\/me\s*/
@@ -11,19 +11,35 @@ const Wrapper = styled.div`
   }
 `
 
+function getHighlightClass(messageType: MessageType) {
+  switch (messageType) {
+    case 'normal':
+      return ''
+    case 'lfrp':
+      return 'highlight-green'
+    case 'admin':
+      return 'highlight-red'
+    case 'system':
+      return ''
+  }
+}
+
 export function MessageComponent(props: { message: Message }) {
-  const { sender, text } = props.message
+  const { sender, text, type } = props.message
   const isAction = text.trim().startsWith('/me')
   const parsedText = text.replace(actionExp, '')
 
   const actionClass = isAction ? 'text-italic' : ''
+  const highlightClass = getHighlightClass(type)
 
   return (
-    <Wrapper className={`padding ${actionClass}`}>
-      <span className="margin-right">
-        <CharacterName name={sender} />
-      </span>
-      <span className={`preserve-ws`} dangerouslySetInnerHTML={{ __html: parsedText }} />
+    <Wrapper className={`${actionClass}`}>
+      <div className={`padding ${highlightClass}`}>
+        <span className="margin-right">
+          <CharacterName name={sender} />
+        </span>
+        <span className={`preserve-ws`} dangerouslySetInnerHTML={{ __html: parsedText }} />
+      </div>
     </Wrapper>
   )
 }
--- src/message/models/Message.ts
+++ src/message/models/Message.ts
@@ -1,9 +1,7 @@
+export type MessageType = 'normal' | 'lfrp' | 'admin' | 'system'
+
 export class Message {
   date = new Date()
 
-  constructor(
-    public sender: string,
-    public text: string,
-    public type: 'normal' | 'lfrp' | 'admin' | 'system',
-  ) {}
+  constructor(public sender: string, public text: string, public type: MessageType) {}
 }
