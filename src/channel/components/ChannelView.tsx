29ce9437b4275a3608d577ca5b14504f687d61b6
kingdaro <kingdaro@gmail.com>
2017-11-01 03:41:14 -0400

    make methods/properties private where appropriate

--- src/channel/components/ChannelView.tsx
+++ src/channel/components/ChannelView.tsx
@@ -74,103 +74,8 @@ function storesToProps(stores: Stores, props: Props): InjectedProps {
 @inject(storesToProps)
 @observer
 class ChannelViewComponent extends React.Component<Props & InjectedProps> {
-  @observable displayedMode = 'both' as ChannelMode
-  infoDrawer = new OverlayState()
-
-  @action
-  setDisplayedMode(mode: ChannelMode) {
-    this.displayedMode = mode
-  }
-
-  @computed
-  get parsedDescription() {
-    return { __html: parseBBC(this.props.channel.description) }
-  }
-
-  @computed
-  get filteredMessages() {
-    const { messages } = this.props.channel
-
-    if (this.props.channel.mode !== 'both') {
-      return messages
-    }
-
-    if (this.displayedMode === 'ads') {
-      return messages.filter(msg => msg.type === 'lfrp')
-    }
-    if (this.displayedMode === 'chat') {
-      return messages.filter(msg => msg.type === 'normal')
-    }
-
-    return messages
-  }
-
-  renderModeFilter(mode: ChannelMode, text: string) {
-    return (
-      <ChannelModeFilter
-        key={mode}
-        onClick={preventDefault(() => this.setDisplayedMode(mode))}
-        text={text}
-        active={this.displayedMode === mode}
-      />
-    )
-  }
-
-  renderHeader() {
-    const { channel } = this.props
-
-    return (
-      <HeaderContainer>
-        <ChatHeader>
-          <div className="flex-row flex-align-center">
-            <h3 className="flex-grow">{channel.title}</h3>
-            <div className="flex-row">
-              {channel.mode === 'both' && [
-                this.renderModeFilter('chat', 'Chat'),
-                this.renderModeFilter('ads', 'Ads'),
-                this.renderModeFilter('both', 'Both'),
-              ]}
-            </div>
-            <ShowOnMobile>
-              <a href="#" onClick={preventDefault(this.infoDrawer.show)}>
-                <Icon name="more-vert" size={24} />
-              </a>
-            </ShowOnMobile>
-          </div>
-        </ChatHeader>
-      </HeaderContainer>
-    )
-  }
-
-  renderDescription() {
-    return (
-      <Description
-        className="bg-color-main scroll-v padding preserve-ws"
-        dangerouslySetInnerHTML={this.parsedDescription}
-      />
-    )
-  }
-
-  renderInfoDrawer() {
-    const { channel } = this.props
-
-    return (
-      <Drawer side="right" visible={this.infoDrawer.isOpen} onShadeClicked={this.infoDrawer.hide}>
-        <div className="scroll-v" style={{ width: '240px' }}>
-          <h3 className="padding">Description</h3>
-          <div
-            className="bg-color-darken-1 padding preserve-ws"
-            dangerouslySetInnerHTML={this.parsedDescription}
-          />
-
-          <h3 className="padding">Users ({channel.getUserCount()})</h3>
-          <div className="bg-color-darken-1">
-            <ChannelUsers users={channel.getUsers()} ops={channel.ops} />
-          </div>
-        </div>
-      </Drawer>
-    )
-  }
+  @observable private displayedMode = 'both' as ChannelMode
+  private infoDrawer = new OverlayState()
 
   render() {
     const { channel, className = '' } = this.props
@@ -200,7 +105,102 @@ class ChannelViewComponent extends React.Component<Props & InjectedProps> {
     )
   }
 
-  renderMessage = (message: Message, i: number) => {
+  @action
+  private setDisplayedMode(mode: ChannelMode) {
+    this.displayedMode = mode
+  }
+
+  @computed
+  private get parsedDescription() {
+    return { __html: parseBBC(this.props.channel.description) }
+  }
+
+  @computed
+  private get filteredMessages() {
+    const { messages } = this.props.channel
+
+    if (this.props.channel.mode !== 'both') {
+      return messages
+    }
+
+    if (this.displayedMode === 'ads') {
+      return messages.filter(msg => msg.type === 'lfrp')
+    }
+    if (this.displayedMode === 'chat') {
+      return messages.filter(msg => msg.type === 'normal')
+    }
+
+    return messages
+  }
+
+  private renderModeFilter(mode: ChannelMode, text: string) {
+    return (
+      <ChannelModeFilter
+        key={mode}
+        onClick={preventDefault(() => this.setDisplayedMode(mode))}
+        text={text}
+        active={this.displayedMode === mode}
+      />
+    )
+  }
+
+  private renderHeader() {
+    const { channel } = this.props
+
+    return (
+      <HeaderContainer>
+        <ChatHeader>
+          <div className="flex-row flex-align-center">
+            <h3 className="flex-grow">{channel.title}</h3>
+            <div className="flex-row">
+              {channel.mode === 'both' && [
+                this.renderModeFilter('chat', 'Chat'),
+                this.renderModeFilter('ads', 'Ads'),
+                this.renderModeFilter('both', 'Both'),
+              ]}
+            </div>
+            <ShowOnMobile>
+              <a href="#" onClick={preventDefault(this.infoDrawer.show)}>
+                <Icon name="more-vert" size={24} />
+              </a>
+            </ShowOnMobile>
+          </div>
+        </ChatHeader>
+      </HeaderContainer>
+    )
+  }
+
+  private renderDescription() {
+    return (
+      <Description
+        className="bg-color-main scroll-v padding preserve-ws"
+        dangerouslySetInnerHTML={this.parsedDescription}
+      />
+    )
+  }
+
+  private renderInfoDrawer() {
+    const { channel } = this.props
+
+    return (
+      <Drawer side="right" visible={this.infoDrawer.isOpen} onShadeClicked={this.infoDrawer.hide}>
+        <div className="scroll-v" style={{ width: '240px' }}>
+          <h3 className="padding">Description</h3>
+          <div
+            className="bg-color-darken-1 padding preserve-ws"
+            dangerouslySetInnerHTML={this.parsedDescription}
+          />
+
+          <h3 className="padding">Users ({channel.getUserCount()})</h3>
+          <div className="bg-color-darken-1">
+            <ChannelUsers users={channel.getUsers()} ops={channel.ops} />
+          </div>
+        </div>
+      </Drawer>
+    )
+  }
+
+  private renderMessage = (message: Message, i: number) => {
     return <MessageComponent key={i} message={message} />
   }
 }
