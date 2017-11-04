3dccc8eb002c5a12d58c7a258529b5e714e398dd
kingdaro <kingdaro@gmail.com>
2017-11-04 13:43:10 -0400

    AuthStore: add actions to complement the observables

--- src/auth/stores/AuthStore.ts
+++ src/auth/stores/AuthStore.ts
@@ -1,5 +1,5 @@
 import * as forage from 'localforage'
-import { observable } from 'mobx'
+import { action, observable } from 'mobx'
 import * as api from 'src/api'
 
 const storageKeyAuth = 'AuthStore_auth'
@@ -14,22 +14,37 @@ export class AuthStore {
   @observable ticket = ''
   @observable characters = [] as string[]
 
+  @action
+  setAccount(account: string) {
+    this.account = account
+  }
+
+  @action
+  setTicket(ticket: string) {
+    this.ticket = ticket
+  }
+
+  @action
+  setCharacters(characters: string[]) {
+    this.characters = characters
+  }
+
   async fetchTicket(account: string, password: string) {
     const ticket = await api.fetchTicket(account, password)
-    this.account = account
-    this.ticket = ticket
+    this.setAccount(account)
+    this.setTicket(ticket)
   }
 
   async fetchCharacters() {
     const { account, ticket } = this
-    this.characters = await api.fetchCharacterList(account, ticket)
+    this.setCharacters(await api.fetchCharacterList(account, ticket))
   }
 
   async loadAuthData() {
     const auth = await forage.getItem<AuthData>(storageKeyAuth)
     if (auth) {
-      this.account = auth.account
-      this.ticket = auth.ticket
+      this.setAccount(auth.account)
+      this.setTicket(auth.ticket)
     }
   }
 
