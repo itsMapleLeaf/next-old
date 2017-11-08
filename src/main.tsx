edcae161221d3c561c2971909aef527ecd83c5ad
kingdaro <kingdaro@gmail.com>
2017-11-08 14:27:19 -0500

    wrap app init in main

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,6 @@ import * as ReactDOM from "react-dom"
 import { App } from "./app/components/App"
 import { stores } from "./stores"
 
-useStrict(true)
-
 const devmode = process.env.NODE_ENV !== "production"
 
 function Root() {
@@ -27,13 +25,18 @@ function render() {
   ReactDOM.render(<Root />, document.getElementById("root"))
 }
 
-render()
-stores.appStore.init().catch(console.error)
+function main() {
+  useStrict(true)
+  render()
+  stores.appStore.init().catch(console.error)
 
-if (devmode) {
-  if (module.hot) {
-    module.hot.accept("./app/components/App", render)
+  if (devmode) {
+    if (module.hot) {
+      module.hot.accept("./app/components/App", render)
+    }
+
+    ;(window as any).stores = stores
   }
-
-  ;(window as any).stores = stores
 }
+
+main()
