2b41eacbc03fed2857762a308c513baaeb7c9c04
kingdaro <kingdaro@gmail.com>
2017-11-15 20:36:23 -0500

    StatusMenu: rewrite with formik

--- src/chat/components/StatusMenu.tsx
+++ src/chat/components/StatusMenu.tsx
@@ -1,23 +1,21 @@
-import { action, observable } from 'mobx'
+import { bind } from 'decko'
+import { Formik, FormikProps } from 'formik'
 import { inject, observer } from 'mobx-react'
 import * as React from 'react'
 import { updateStatus } from 'src/chat/actions'
-import { preventDefault } from 'src/common/util/react'
 import { Stores } from 'src/stores'
-import { Select, Button, TextArea } from 'src/ui/components'
+import { Button, Select, TextArea } from 'src/ui/components'
 
 type InjectedProps = {
-  initialStatus: string
-  initialStatusMessage: string
+  initialValues: FormValues
   onSubmit: (status: string, statusMessage: string) => void
 }
 
 function storesToProps(stores: Stores): InjectedProps {
   const { chatStore, chatViewStore } = stores
-  const char = chatStore.identityCharacter
+  const { status, statusMessage } = chatStore.identityCharacter
   return {
-    initialStatus: char.status,
-    initialStatusMessage: char.statusMessage,
+    initialValues: { status, statusMessage },
     onSubmit(status: string, statusMessage: string) {
       updateStatus(status, statusMessage)
       chatViewStore.statusMenu.hide()
@@ -25,67 +23,60 @@ function storesToProps(stores: Stores): InjectedProps {
   }
 }
 
-// TODO: rewrite with formik
+type FormValues = {
+  status: string
+  statusMessage: string
+}
+
 @inject(storesToProps)
 @observer
 class StatusMenuComponent extends React.Component<InjectedProps> {
-  @observable status = ''
-  @observable statusMessage = ''
-
-  @action.bound
-  updateStatus(event: React.ChangeEvent<HTMLSelectElement>) {
-    this.status = event.currentTarget.value
-  }
-
-  @action.bound
-  updateStatusMessage(event: React.UIEvent<HTMLTextAreaElement>) {
-    this.statusMessage = event.currentTarget.value
-  }
-
-  @action.bound
-  handleSubmit() {
-    this.props.onSubmit(this.status, this.statusMessage)
-  }
-
-  @action
-  init() {
-    this.status = this.props.initialStatus
-    this.statusMessage = this.props.initialStatusMessage
-  }
-
-  componentDidMount() {
-    this.init()
-  }
-
   render() {
     return (
       <div className="bg-color-main">
-        <form onSubmit={preventDefault(this.handleSubmit)}>
-          <fieldset>
-            <h2 style={{ margin: 0 }}>Status Update</h2>
-          </fieldset>
-          <fieldset>
-            <Select value={this.status} onChange={this.updateStatus}>
-              <option value="online">Online</option>
-              <option value="looking">Looking</option>
-              <option value="away">Away</option>
-              <option value="busy">Busy</option>
-              <option value="dnd">DND</option>
-            </Select>
-          </fieldset>
-          <fieldset>
-            <TextArea
-              placeholder="Status message..."
-              value={this.statusMessage}
-              onInput={this.updateStatusMessage}
-            />
-          </fieldset>
-          <fieldset>
-            <Button type="submit">Update</Button>
-          </fieldset>
-        </form>
+        <Formik
+          initialValues={this.props.initialValues}
+          render={this.renderForm}
+          onSubmit={this.handleSubmit}
+        />
       </div>
     )
+  }
+
+  @bind
+  private renderForm(props: FormikProps<FormValues>) {
+    return (
+      <form onSubmit={props.handleSubmit}>
+        <fieldset>
+          <h2 style={{ margin: 0 }}>Status Update</h2>
+        </fieldset>
+        <fieldset>
+          <Select name="status" value={props.values.status} onChange={props.handleChange}>
+            <option value="online">Online</option>
+            <option value="looking">Looking</option>
+            <option value="away">Away</option>
+            <option value="busy">Busy</option>
+            <option value="dnd">DND</option>
+          </Select>
+        </fieldset>
+        <fieldset>
+          <TextArea
+            name="statusMessage"
+            placeholder="Status message..."
+            value={props.values.statusMessage}
+            onInput={props.handleChange}
+          />
+        </fieldset>
+        <fieldset>
+          <Button type="submit">Update</Button>
+        </fieldset>
+      </form>
+    )
+  }
+
+  @bind
+  private handleSubmit(values: FormValues) {
+    this.props.onSubmit(values.status, values.statusMessage)
   }
 }
 
