d5e7a270bfc36ecbf502cf950f087efb87b32412
kingdaro <kingdaro@gmail.com>
2017-11-18 12:38:06 -0500

    added some extra spice to fade transition

--- src/common/components/FadeTransition.tsx
+++ src/common/components/FadeTransition.tsx
@@ -7,5 +7,6 @@ type Props = {
 export const FadeTransition = styled.div`
   opacity: ${(props: Props) => (props.visible ? 1 : 0)};
   visibility: ${(props: Props) => (props.visible ? 'visible' : 'hidden')};
+  transform: ${(props: Props) => `translateY(${props.visible ? 0 : '-10px'})`};
   transition: 0.3s;
 `
